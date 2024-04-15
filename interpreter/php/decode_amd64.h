45fc48f1497ccea87635ae4726e82bbb1b095e39
Christos Kalkanis <christos.kalkanis@elastic.co>
2024-04-15 15:12:44 -0400

    license: Add preamble to non-eBPF C/include files
    The eBPF code in this repository is licensed under GPL.

--- interpreter/php/decode_amd64.h
+++ interpreter/php/decode_amd64.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Apache License 2.0.
+ * See the file "LICENSE" for details.
+ */
+
 //go:build amd64
 #ifndef __INCLUDED_PHP_DECODE_X86_64__
 #define __INCLUDED_PHP_DECODE_X86_64__
