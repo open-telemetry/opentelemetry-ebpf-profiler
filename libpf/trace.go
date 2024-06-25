89fe9457b972cf423744602c8229fe6406ad1b5a
Joel Höner <joel@elastic.co>
2024-06-25 17:17:13 +0200

    Properly populate reported mappings with ELF VAs

--- libpf/trace.go
+++ libpf/trace.go
@@ -10,15 +10,27 @@ package libpf
 // stack frame via the file ID and line number at the offset i in the trace. The
 // information for the most recently called function is at offset 0.
 type Trace struct {
-	Files      []FileID
-	Linenos    []AddressOrLineno
-	FrameTypes []FrameType
-	Hash       TraceHash
+	Files              []FileID
+	Linenos            []AddressOrLineno
+	FrameTypes         []FrameType
+	MappingStart       []Address
+	MappingEnd         []Address
+	MappingFileOffsets []uint64
+	Hash               TraceHash
 }
 
 // AppendFrame appends a frame to the columnar frame array.
 func (trace *Trace) AppendFrame(ty FrameType, file FileID, addrOrLine AddressOrLineno) {
+	trace.AppendFrameFull(ty, file, addrOrLine, 0, 0, 0)
+}
+
+// AppendFrameFull appends a frame with mapping info to the columnar frame array.
+func (trace *Trace) AppendFrameFull(ty FrameType, file FileID, addrOrLine AddressOrLineno,
+	mappingStart Address, mappingEnd Address, mappingFileOffset uint64) {
 	trace.FrameTypes = append(trace.FrameTypes, ty)
 	trace.Files = append(trace.Files, file)
 	trace.Linenos = append(trace.Linenos, addrOrLine)
+	trace.MappingStart = append(trace.MappingStart, mappingStart)
+	trace.MappingEnd = append(trace.MappingEnd, mappingEnd)
+	trace.MappingFileOffsets = append(trace.MappingFileOffsets, mappingFileOffset)
 }
