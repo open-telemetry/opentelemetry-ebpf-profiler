bad85ce021e81d9d83d850d542b6ebd3f24a3b03
girodav <1390902+girodav@users.noreply.github.com>
2024-08-07 10:37:48 +0100

    Fix duplicate entries in Profile.attribute_table

--- reporter/otlp_reporter.go
+++ reporter/otlp_reporter.go
@@ -503,6 +503,10 @@ func (r *OTLPReporter) getProfile() (profile *profiles.Profile, startTS, endTS u
 	funcMap := make(map[funcInfo]uint64)
 	funcMap[funcInfo{name: "", fileName: ""}] = 0
 
+	// attributeMap is a temporary helper that will build the AttributeTable.
+	// This is to ensure that AttributeTable does not contain duplicates
+	attributeMap := make(map[string]uint64)
+
 	numSamples := len(samples)
 	profile = &profiles.Profile{
 		// SampleType - Next step: Figure out the correct SampleType.
@@ -668,7 +672,7 @@ func (r *OTLPReporter) getProfile() (profile *profiles.Profile, startTS, endTS u
 			profile.Location = append(profile.Location, loc)
 		}
 
-		sample.Attributes = getSampleAttributes(profile, traceKey)
+		sample.Attributes = getSampleAttributes(profile, traceKey, attributeMap)
 		sample.LocationsLength = uint64(len(traceInfo.frameTypes))
 		locationIndex += sample.LocationsLength
 
@@ -738,19 +742,26 @@ func createFunctionEntry(funcMap map[funcInfo]uint64,
 }
 
 // getSampleAttributes builds a sample-specific list of attributes.
-func getSampleAttributes(profile *profiles.Profile, k traceAndMetaKey) []uint64 {
+func getSampleAttributes(profile *profiles.Profile, k traceAndMetaKey,
+	attributeMap map[string]uint64) []uint64 {
 	indices := make([]uint64, 0, 4)
 
 	addAttr := func(k attribute.Key, v string) {
 		if v == "" {
 			return
 		}
-
-		indices = append(indices, uint64(len(profile.AttributeTable)))
+		tempKey := string(k) + "_" + v
+		if tempKeyIndex, exists := attributeMap[tempKey]; exists {
+			indices = append(indices, tempKeyIndex)
+			return
+		}
+		newIndex := uint64(len(profile.AttributeTable))
+		indices = append(indices, newIndex)
 		profile.AttributeTable = append(profile.AttributeTable, &common.KeyValue{
 			Key:   string(k),
 			Value: &common.AnyValue{Value: &common.AnyValue_StringValue{StringValue: v}},
 		})
+		attributeMap[tempKey] = newIndex
 	}
 
 	addAttr(semconv.ContainerIDKey, k.containerID)
