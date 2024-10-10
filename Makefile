open-telemetry/opentelemetry-ebpf-profiler

commit e7a70381ba9c788280d5eaea5ae95628cc011e07

Use go.opentelemetry.io vanity URL (#164)

Makefile
@@ -66,12 +66,22 @@ ebpf:
 	$(MAKE) -j$(shell nproc) -C support/ebpf
 
 GOLANGCI_LINT_VERSION = "v1.60.1"
-lint: generate
+lint: generate vanity-import-check
 	go run github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION) version
 	go run github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION) run
 
 linter-version:
 	@echo $(GOLANGCI_LINT_VERSION)
+
+.PHONY: vanity-import-check
+vanity-import-check:
+	@go install github.com/jcchavezs/porto/cmd/porto@latest
+	@porto --include-internal -l . || ( echo "(run: make vanity-import-fix)"; exit 1 )
+
+.PHONY: vanity-import-fix
+vanity-import-fix: $(PORTO)
+	@go install github.com/jcchavezs/porto/cmd/porto@latest
+	@porto --include-internal -w .
 
 test: generate ebpf test-deps
 	go test $(GO_FLAGS) ./...
@@ -108,5 +118,5 @@ legal:
 	@go-licenses save --force . --save_path=LICENSES
 	@./legal/add-non-go.sh legal/non-go-dependencies.json LICENSES
 
-codespell: 
+codespell:
 	@codespell

commit 445391ae7402e0caab0678f6d1fdfe72795bd090

Update build
Fixes vc breakage after #164.

Makefile
@@ -39,9 +39,9 @@ VERSION ?= v0.0.0
 BUILD_TIMESTAMP ?= $(shell date +%s)
 REVISION ?= $(BRANCH)-$(COMMIT_SHORT_SHA)
 
-LDFLAGS := -X github.com//open-telemetry/opentelemetry-ebpf-profiler/vc.version=$(VERSION) \
-	-X github.com/open-telemetry/opentelemetry-ebpf-profiler/vc.revision=$(REVISION) \
-	-X github.com/open-telemetry/opentelemetry-ebpf-profiler/vc.buildTimestamp=$(BUILD_TIMESTAMP) \
+LDFLAGS := -X go.opentelemetry.io/ebpf-profiler/vc.version=$(VERSION) \
+	-X go.opentelemetry.io/ebpf-profiler/vc.revision=$(REVISION) \
+	-X go.opentelemetry.io/ebpf-profiler/vc.buildTimestamp=$(BUILD_TIMESTAMP) \
 	-extldflags=-static
 
 GO_FLAGS := -buildvcs=false -ldflags="$(LDFLAGS)" -tags osusergo,netgo