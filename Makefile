open-telemetry/opentelemetry-ebpf-profiler

commit e7a70381ba9c788280d5eaea5ae95628cc011e07

Use go.opentelemetry.io vanity URL (#164)

Makefile
@@ -66,12 +66,22 @@ ebpf:
 	$(MAKE) -j$(shell nproc) -C support/ebpf
 
 GOLANGCI_LINT_VERSION = "v1.60.1"
-lint: generate
+lint: generate vanity-import-check
 	go run github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION) version
 	go run github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANGCI_LINT_VERSION) run
 
 linter-version:
 	@echo $(GOLANGCI_LINT_VERSION)
+
+.PHONY: vanity-import-check
+vanity-import-check:
+	@go install github.com/jcchavezs/porto/cmd/porto@latest
+	@porto --include-internal -l . || ( echo "(run: make vanity-import-fix)"; exit 1 )
+
+.PHONY: vanity-import-fix
+vanity-import-fix: $(PORTO)
+	@go install github.com/jcchavezs/porto/cmd/porto@latest
+	@porto --include-internal -w .
 
 test: generate ebpf test-deps
 	go test $(GO_FLAGS) ./...
@@ -108,5 +118,5 @@ legal:
 	@go-licenses save --force . --save_path=LICENSES
 	@./legal/add-non-go.sh legal/non-go-dependencies.json LICENSES
 
-codespell: 
+codespell:
 	@codespell

cli_flags.go
@@ -12,7 +12,7 @@ import (
 	"github.com/peterbourgon/ff/v3"
 	log "github.com/sirupsen/logrus"
 
-	"github.com/open-telemetry/opentelemetry-ebpf-profiler/tracer"
+	"go.opentelemetry.io/ebpf-profiler/tracer"
 )
 
 const (

commit 40ffc8187467835e5937678e269f615ba660e666

Update README (#179)
- Updated flags for new binary name (ebpf-profiler)
- Updated build
- Fixed vc breakage after #164

Makefile
@@ -39,9 +39,9 @@ VERSION ?= v0.0.0
 BUILD_TIMESTAMP ?= $(shell date +%s)
 REVISION ?= $(BRANCH)-$(COMMIT_SHORT_SHA)
 
-LDFLAGS := -X github.com//open-telemetry/opentelemetry-ebpf-profiler/vc.version=$(VERSION) \
-	-X github.com/open-telemetry/opentelemetry-ebpf-profiler/vc.revision=$(REVISION) \
-	-X github.com/open-telemetry/opentelemetry-ebpf-profiler/vc.buildTimestamp=$(BUILD_TIMESTAMP) \
+LDFLAGS := -X go.opentelemetry.io/ebpf-profiler/vc.version=$(VERSION) \
+	-X go.opentelemetry.io/ebpf-profiler/vc.revision=$(REVISION) \
+	-X go.opentelemetry.io/ebpf-profiler/vc.buildTimestamp=$(BUILD_TIMESTAMP) \
 	-extldflags=-static
 
 GO_TAGS := osusergo,netgo

cli_flags.go
@@ -93,7 +93,7 @@ type arguments struct {
 func parseArgs() (*arguments, error) {
 	var args arguments
 
-	fs := flag.NewFlagSet("opentelemetry-ebpf-profiler", flag.ExitOnError)
+	fs := flag.NewFlagSet("ebpf-profiler", flag.ExitOnError)
 
 	// Please keep the parameters ordered alphabetically in the source-code.
 	fs.UintVar(&args.bpfVerifierLogLevel, "bpf-log-level", 0, bpfVerifierLogLevelHelp)