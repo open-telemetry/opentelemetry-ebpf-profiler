5fb721255b70caa8d011a5da9da8221fd5161a64
Tim Rühsen <tim.ruhsen@elastic.co>
2024-10-18 10:45:07 +0200

    Remove test for measuring memory consumption

--- proc/proc_test.go
+++ proc/proc_test.go
@@ -4,8 +4,6 @@
 package proc
 
 import (
-	"fmt"
-	"runtime"
 	"testing"
 
 	"go.opentelemetry.io/ebpf-profiler/libpf"
@@ -39,30 +37,4 @@ func TestParseKallSyms(t *testing.T) {
 
 	assertSymbol(t, symmap, "cpu_tss_rw", 0x6000)
 	assertSymbol(t, symmap, "hid_add_device", 0xffffffffc033e550)
-}
-
-func printMemUsage() {
-	runtime.GC()
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
-}
-
-func TestGetKallsymsMemoryUsage(t *testing.T) {
-	fmt.Println("Memory usage before calling GetKallsyms:")
-	printMemUsage()
-
-	symmap, err := GetKallsyms("/proc/kallsyms")
-	require.NoError(t, err)
-	require.NotNil(t, symmap)
-
-	fmt.Println("Memory usage after calling GetKallsyms:")
-	printMemUsage()
 }
