9560d14abab90c7bb85089abc8939463242f1244
dmathieu <42@dmathieu.com>
2024-10-24 14:49:58 +0200

    fix linter

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -172,15 +172,14 @@ func (c *Controller) Start(ctx context.Context) error {
 
 // Shutdown stops the controller
 func (c *Controller) Shutdown() {
-	log.Info("Stop processing ...")	
+	log.Info("Stop processing ...")
 	if c.reporter != nil {
 		c.reporter.Stop()
 	}
-	
+
 	if c.tracer != nil {
 		c.tracer.Close()
 	}
-
 }
 
 func startTraceHandling(ctx context.Context, rep reporter.TraceReporter,
