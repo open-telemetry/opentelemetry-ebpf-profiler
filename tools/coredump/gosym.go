6e99bdd48398aef579fffbec92bc0129b942699e
Felix Geisendörfer <felix@felixge.de>
2024-11-10 18:47:53 +0100

    coredump/gosym: use debug/gosym instead of addr2line

--- tools/coredump/gosym.go
+++ tools/coredump/gosym.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"context"
+	"debug/elf"
+	"debug/gosym"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 	"go.opentelemetry.io/ebpf-profiler/libpf"
@@ -56,13 +54,13 @@ func (cmd *gosymCmd) exec(context.Context, []string) (err error) {
 		return fmt.Errorf("failed to find go module addresses: %w", err)
 	}
 
-	binary, err := extractModuleToTempFile(cmd.store, module)
+	goBinary, err := cmd.store.OpenReadAt(module.Ref)
 	if err != nil {
-		return fmt.Errorf("failed to extract binary: %w", err)
+		return fmt.Errorf("failed to open module: %w", err)
 	}
-	defer os.Remove(binary)
+	defer goBinary.Close()
 
-	locs, err := goSymbolize(binary, addrs)
+	locs, err := goSymbolize(goBinary, addrs)
 	if err != nil {
 		return fmt.Errorf("failed to symbolize: %w", err)
 	}
@@ -124,122 +122,41 @@ func goModuleAddrs(c *CoredumpTestCase) (*ModuleInfo, map[libpf.AddressOrLineno]
 	return maxAddrs.module, maxAddrs.addrs, nil
 }
 
-func extractModuleToTempFile(store *modulestore.Store, m *ModuleInfo) (string, error) {
-	file, err := os.CreateTemp("", "")
-	if err != nil {
-		return "", err
-	}
-	return file.Name(), store.UnpackModule(m.Ref, file)
-}
-
 type addrSet[T any] map[libpf.AddressOrLineno]T
 
-func goSymbolize[T any](binary string, addrs addrSet[T]) (map[libpf.AddressOrLineno]*reporter.FrameMetadataArgs, error) {
-	// Launch addr2line process.
-	addr2line := exec.Command("go", "tool", "addr2line", binary)
-	inR, inW := io.Pipe()
-	outR, outW := io.Pipe()
-	addr2line.Stdin = inR
-	addr2line.Stdout = outW
-	addr2line.Stderr = outW
-	if err := addr2line.Start(); err != nil {
-		return nil, err
-	}
-
-	// Transform addrs into a list. This allows us to figure out which addr2line
-	// output corresponds to which address.
-	addrList := make([]libpf.AddressOrLineno, 0, len(addrs))
-	for pc, _ := range addrs {
-		addrList = append(addrList, pc)
-	}
-
-	// Parse addr2line output and map it to addrs we were given.
-	frames := map[libpf.AddressOrLineno]*reporter.FrameMetadataArgs{}
-	scanCh := make(chan error)
-	go func() {
-		// Drain the output pipe in case we hit a parsing error.
-		defer io.Copy(io.Discard, outR)
-
-		var err error
-		readFrame := addr2LineFrameReader(outR)
-		for {
-			var frame *reporter.FrameMetadataArgs
-			frame, err = readFrame()
-			if err != nil {
-				break
-			}
-			addr := addrList[len(frames)]
-			frames[addr] = frame
-		}
-		scanCh <- err
-	}()
-
-	// Write addrList to addr2line stdin.
-	var writeErr error
-	writeAddr := addr2LineAddrWriter(inW)
-	for _, addr := range addrList {
-		if writeErr = writeAddr(addr); writeErr != nil {
-			break
-		}
-	}
-
-	// Close the input pipe to signal addr2line that we're done.
-	if err := inW.Close(); err != nil {
-		return nil, err
-		// Wait for addr2line to finish.
-	} else if err := addr2line.Wait(); err != nil {
-		return nil, err
-		// Signal the output reader that we're done.
-	} else if err := outW.Close(); err != nil {
-		return nil, err
-		// Wait for the output reader to finish.
-	} else if err := <-scanCh; err != nil && err != io.EOF {
-		return nil, err
-	}
-	return frames, writeErr
-}
-
-func addr2LineAddrWriter(w io.Writer) func(libpf.AddressOrLineno) error {
-	return func(addr libpf.AddressOrLineno) error {
-		_, err := fmt.Fprintf(w, "%x\n", addr)
-		return err
-	}
-}
-
-func addr2LineFrameReader(r io.Reader) func() (*reporter.FrameMetadataArgs, error) {
-	scanner := bufio.NewScanner(r)
-	scanErr := func() error {
-		if err := scanner.Err(); err != nil {
-			return err
-		}
-		return io.EOF
-	}
-	var pair [2]string
-	return func() (*reporter.FrameMetadataArgs, error) {
-		if !scanner.Scan() {
-			return nil, scanErr()
-		}
-		pair[0] = scanner.Text()
-		if !scanner.Scan() {
-			return nil, fmt.Errorf("expected second line, but got: %w", scanErr())
-		}
-		pair[1] = scanner.Text()
-		return linePairToFrame(pair)
-	}
-}
-
-func linePairToFrame(pair [2]string) (*reporter.FrameMetadataArgs, error) {
-	var frame reporter.FrameMetadataArgs
-	frame.FunctionName = pair[0]
-	file, line, found := strings.Cut(pair[1], ":")
-	if !found {
-		return nil, fmt.Errorf("expected file:line but got: %q", pair[1])
-	}
-	lineNum, err := strconv.Atoi(line)
+func goSymbolize[T any](goBinary io.ReaderAt, addrs addrSet[T]) (map[libpf.AddressOrLineno]*reporter.FrameMetadataArgs, error) {
+	exe, err := elf.NewFile(goBinary)
 	if err != nil {
-		return nil, fmt.Errorf("invalid line number: %q", line)
+		return nil, err
 	}
-	frame.SourceFile = file
-	frame.SourceLine = libpf.SourceLineno(lineNum)
-	return &frame, nil
+
+	lineTableData, err := exe.Section(".gopclntab").Data()
+	if err != nil {
+		return nil, err
+	}
+	lineTable := gosym.NewLineTable(lineTableData, exe.Section(".text").Addr)
+	if err != nil {
+		return nil, err
+	}
+
+	symTableData, err := exe.Section(".gosymtab").Data()
+	if err != nil {
+		return nil, err
+	}
+
+	symTable, err := gosym.NewTable(symTableData, lineTable)
+	if err != nil {
+		return nil, err
+	}
+
+	frames := map[libpf.AddressOrLineno]*reporter.FrameMetadataArgs{}
+	for addr, _ := range addrs {
+		file, line, fn := symTable.PCToLine(uint64(addr))
+		frames[addr] = &reporter.FrameMetadataArgs{
+			FunctionName: fn.Name,
+			SourceFile:   file,
+			SourceLine:   libpf.SourceLineno(line),
+		}
+	}
+	return frames, nil
 }
