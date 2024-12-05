3a0fd6c254fe24442f1638e3538bed5f79e6a272
Frederic Branczyk <fbranczyk@gmail.com>
2024-12-05 11:28:15 +0100

    proc: Fix parsing Kernel Module lines where refcount is "-"

--- interpreter/loaderinfo.go
+++ interpreter/loaderinfo.go
@@ -55,7 +55,7 @@ func (i *LoaderInfo) GetSymbolAsRanges(symbol libpf.SymbolName) ([]util.Range, e
 	start := uint64(sym.Address)
 	return []util.Range{{
 		Start: start,
-		End:   start + uint64(sym.Size)},
+		End:   start + sym.Size},
 	}, nil
 }
 
