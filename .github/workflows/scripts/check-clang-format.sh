68cd499275f451fc183e20e2b0306b06bb873eaf
Florian Lehner <florian.lehner@elastic.co>
2025-01-16 16:34:55 +0100

    fixup: simplify script
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- .github/workflows/scripts/check-clang-format.sh
+++ .github/workflows/scripts/check-clang-format.sh
@@ -3,33 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-CLANG_FORMAT_VERSION="clang-format-17"
-if ! command -v ${CLANG_FORMAT_VERSION}
-then
-  echo "ERROR: requires ${CLANG_FORMAT_VERSION}"
-  exit 1
-fi
-
-RC=0
-CMD="${CLANG_FORMAT_VERSION} -Werror --dry-run -style=file"
-function check_file
-{
-  if ! ${CMD} $1
-  then
-    RC=1
-  fi
-}
-
-# Check that C and C++ source files are properly clang-formatted
-FILES=$(find ./support/ebpf \
-	-type f                                                           \
-	\( -name "*.c"                                                    \
-	-o -name "*.h"   \)                                               \
-	-print)
-
-for FILE in ${FILES}
-do
-  check_file ${FILE}
-done
-
-exit ${RC}
+CLANG_FORMAT="clang-format-17"
+${CLANG_FORMAT} --version
+${CLANG_FORMAT} -Werror --dry-run -style=file ./support/ebpf/*.[ch]
