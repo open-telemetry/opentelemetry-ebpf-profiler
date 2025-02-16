ee9ee2f24287b3edc980054e5bb543d9b8e18d00
Greg Mefford <greg@gregmefford.com>
2025-02-16 14:26:05 -0500

    Fix formatting on generated types.go

--- support/types.go
+++ support/types.go
@@ -7,39 +7,39 @@
 package support // import "go.opentelemetry.io/ebpf-profiler/support"
 
 const (
-	FrameMarkerUnknown  = 0x0
-	FrameMarkerErrorBit = 0x80
-	FrameMarkerPython   = 0x1
-	FrameMarkerNative   = 0x3
-	FrameMarkerPHP      = 0x2
-	FrameMarkerPHPJIT   = 0x9
-	FrameMarkerKernel   = 0x4
-	FrameMarkerHotSpot  = 0x5
-	FrameMarkerRuby     = 0x6
-	FrameMarkerPerl     = 0x7
-	FrameMarkerV8       = 0x8
-	FrameMarkerDotnet   = 0xa
-	FrameMarkerBeam     = 0xb
-	FrameMarkerAbort    = 0xff
+	FrameMarkerUnknown	= 0x0
+	FrameMarkerErrorBit	= 0x80
+	FrameMarkerPython	= 0x1
+	FrameMarkerNative	= 0x3
+	FrameMarkerPHP		= 0x2
+	FrameMarkerPHPJIT	= 0x9
+	FrameMarkerKernel	= 0x4
+	FrameMarkerHotSpot	= 0x5
+	FrameMarkerRuby		= 0x6
+	FrameMarkerPerl		= 0x7
+	FrameMarkerV8		= 0x8
+	FrameMarkerDotnet	= 0xa
+	FrameMarkerBeam		= 0xb
+	FrameMarkerAbort	= 0xff
 )
 
 const (
-	ProgUnwindStop    = 0x0
-	ProgUnwindNative  = 0x1
-	ProgUnwindHotspot = 0x2
-	ProgUnwindPython  = 0x4
-	ProgUnwindPHP     = 0x5
-	ProgUnwindRuby    = 0x6
-	ProgUnwindPerl    = 0x3
-	ProgUnwindV8      = 0x7
-	ProgUnwindDotnet  = 0x8
-	ProgUnwindBeam    = 0x9
+	ProgUnwindStop		= 0x0
+	ProgUnwindNative	= 0x1
+	ProgUnwindHotspot	= 0x2
+	ProgUnwindPython	= 0x4
+	ProgUnwindPHP		= 0x5
+	ProgUnwindRuby		= 0x6
+	ProgUnwindPerl		= 0x3
+	ProgUnwindV8		= 0x7
+	ProgUnwindDotnet	= 0x8
+	ProgUnwindBeam		= 0x9
 )
 
 const (
-	DeltaCommandFlag = 0x8000
+	DeltaCommandFlag	= 0x8000
 
-	MergeOpcodeNegative = 0x80
+	MergeOpcodeNegative	= 0x80
 )
 
 const (
@@ -53,26 +53,26 @@ const (
 )
 
 const (
-	BitWidthPID  = 0x20
-	BitWidthPage = 0x40
+	BitWidthPID	= 0x20
+	BitWidthPage	= 0x40
 )
 
 const (
-	StackDeltaBucketSmallest = 0x8
-	StackDeltaBucketLargest  = 0x17
+	StackDeltaBucketSmallest	= 0x8
+	StackDeltaBucketLargest		= 0x17
 
-	StackDeltaPageBits = 0x10
-	StackDeltaPageMask = 0xffff
+	StackDeltaPageBits	= 0x10
+	StackDeltaPageMask	= 0xffff
 )
 
 const (
-	HSTSIDIsStubBit       = 0x3f
-	HSTSIDHasFrameBit     = 0x3e
-	HSTSIDStackDeltaBit   = 0x38
-	HSTSIDStackDeltaMask  = 0x3f
-	HSTSIDStackDeltaScale = 0x8
-	HSTSIDSegMapBit       = 0x0
-	HSTSIDSegMapMask      = 0xffffffffffffff
+	HSTSIDIsStubBit		= 0x3f
+	HSTSIDHasFrameBit	= 0x3e
+	HSTSIDStackDeltaBit	= 0x38
+	HSTSIDStackDeltaMask	= 0x3f
+	HSTSIDStackDeltaScale	= 0x8
+	HSTSIDSegMapBit		= 0x0
+	HSTSIDSegMapMask	= 0xffffffffffffff
 )
 
 const (
@@ -80,9 +80,9 @@ const (
 )
 
 const (
-	TraceOriginUnknown  = 0x0
-	TraceOriginSampling = 0x1
-	TraceOriginOffCPU   = 0x2
+	TraceOriginUnknown	= 0x0
+	TraceOriginSampling	= 0x1
+	TraceOriginOffCPU	= 0x2
 )
 
 const OffCPUThresholdMax = 0x3e8
