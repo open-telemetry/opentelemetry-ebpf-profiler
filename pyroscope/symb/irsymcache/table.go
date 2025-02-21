f444f454aefb072c3a787f65c93ae07210517a0e
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-02-21 14:57:50 +0800

    pyroscope: symbol lookup table disk cache

---
+++ pyroscope/symb/irsymcache/table.go
@@ -0,0 +1,23 @@
+package irsymcache
+
+import (
+	"os"
+
+	"go.opentelemetry.io/ebpf-profiler/pyroscope/symb/table"
+)
+
+type TableTableFactory struct {
+	Options []table.Option
+}
+
+func (t TableTableFactory) ConvertTable(src, dst *os.File) error {
+	return table.FDToTable(src, dst, t.Options...)
+}
+
+func (t TableTableFactory) OpenTable(path string) (Table, error) {
+	return table.OpenPath(path, t.Options...)
+}
+
+func (t TableTableFactory) Name() string {
+	return table.VersionName()
+}
