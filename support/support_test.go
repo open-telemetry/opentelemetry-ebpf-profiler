a1dfd0894e14b519da00291b3729cdd14abad29c
Florian Lehner <florian.lehner@elastic.co>
2025-02-25 10:34:15 +0100

    fixup: make linter happy
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- support/support_test.go
+++ support/support_test.go
@@ -14,10 +14,14 @@ func TestSizeOfCGoStruct(t *testing.T) {
 		input uintptr
 		want  uintptr
 	}{
-		{name: "ApmIntProcInfo", input: unsafe.Sizeof(ApmIntProcInfo{}), want: sizeof_ApmIntProcInfo},
-		{name: "DotnetProcInfo", input: unsafe.Sizeof(DotnetProcInfo{}), want: sizeof_DotnetProcInfo},
-		{name: "PHPProcInfo", input: unsafe.Sizeof(PHPProcInfo{}), want: sizeof_PHPProcInfo},
-		{name: "RubyProcInfo", input: unsafe.Sizeof(RubyProcInfo{}), want: sizeof_RubyProcInfo},
+		{name: "ApmIntProcInfo", input: unsafe.Sizeof(ApmIntProcInfo{}),
+			want: sizeof_ApmIntProcInfo},
+		{name: "DotnetProcInfo", input: unsafe.Sizeof(DotnetProcInfo{}),
+			want: sizeof_DotnetProcInfo},
+		{name: "PHPProcInfo", input: unsafe.Sizeof(PHPProcInfo{}),
+			want: sizeof_PHPProcInfo},
+		{name: "RubyProcInfo", input: unsafe.Sizeof(RubyProcInfo{}),
+			want: sizeof_RubyProcInfo},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
