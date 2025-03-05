b8377ce2f931e9cb59fe37ac1db35ab421888b6a
Joel Höner <joel@zystem.io>
2025-03-05 18:19:31 +0100

    fixup: don't leak objfile::File instance

--- rust-crates/symblib-capi/src/gosym.rs
+++ rust-crates/symblib-capi/src/gosym.rs
@@ -3,13 +3,21 @@
 
 use crate::{FfiResult, StatusCode, SymblibPointResolver};
 use std::ffi::{c_char, CStr};
+use std::mem;
 use std::path::Path;
-use symblib::symbconv::PointResolver;
+use symblib::symbconv::{PointResolver, ResolvedSymbol, Result as SymconvResult};
 use symblib::{gosym::GoRuntimeInfo, objfile};
 
 pub struct SymblibGoRuntime {
-    #[allow(dead_code)]
+    #[allow(unused)]
+    obj: Box<objfile::File>,
     runtime: GoRuntimeInfo<'static>,
+}
+
+impl PointResolver for SymblibGoRuntime {
+    fn symbols_for_pc(&self, pc: symblib::VirtAddr) -> SymconvResult<Vec<ResolvedSymbol>> {
+        self.runtime.symbols_for_pc(pc)
+    }
 }
 
 #[no_mangle]
@@ -32,12 +40,20 @@ unsafe fn goruntime_new_impl(
         .map(Path::new)
         .map_err(|_| StatusCode::BadUtf8)?;
 
-    let obj = Box::leak(Box::new(objfile::File::load(executable)?));
+    let obj = Box::new(objfile::File::load(executable)?);
     let obj_reader = obj.parse()?;
     let go_runtime = GoRuntimeInfo::open(&obj_reader)?;
 
+    // Transmute away lifetime to allow for self-referential struct.
+    let go_runtime: GoRuntimeInfo<'static> = mem::transmute(go_runtime);
+
+    let resolver = SymblibGoRuntime {
+        obj,
+        runtime: go_runtime,
+    };
+
     let point_resolver = Box::new(SymblibPointResolver::new(
-        Box::new(go_runtime) as Box<dyn PointResolver + Send>
+        Box::new(resolver) as Box<dyn PointResolver + Send>
     ));
     *runtime = Box::into_raw(point_resolver);
     Ok(())
