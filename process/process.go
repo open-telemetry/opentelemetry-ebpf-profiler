218cdd467005f4ab3c0bb8943ef5a5ad3868ee96
Christos Kalkanis <christos.kalkanis@elastic.co>
2025-03-04 23:09:08 -0500

    Handle processes whose main thread has called pthread_exit
    This allows the agent to continue profiling a process whose main
    thread has exited, but other threads continue to run. Mapping changes
    triggered by one of the remaining threads are also tracked.

--- process/process.go
+++ process/process.go
@@ -29,8 +29,10 @@ import (
 // process that is currently running on this machine.
 type systemProcess struct {
 	pid libpf.PID
+	tid libpf.PID
 
-	remoteMemory remotememory.RemoteMemory
+	mainThreadExit bool
+	remoteMemory   remotememory.RemoteMemory
 
 	fileToMapping map[string]*Mapping
 }
@@ -54,9 +56,10 @@ func init() {
 }
 
 // New returns an object with Process interface accessing it
-func New(pid libpf.PID) Process {
+func New(pid, tid libpf.PID) Process {
 	return &systemProcess{
 		pid:          pid,
+		tid:          tid,
 		remoteMemory: remotememory.NewProcessVirtualMemory(pid),
 	}
 }
@@ -166,9 +169,8 @@ func parseMappings(mapsFile io.Reader) ([]Mapping, uint32, error) {
 				path = VdsoPathName
 				device = 0
 				inode = vdsoInode
-			} else if path != "" {
-				// Ignore [vsyscall] and similar executable kernel
-				// pages we don't care about
+			} else {
+				// Ignore mappings that are invalid, non-existent or are special pseudo-files
 				continue
 			}
 		} else {
@@ -230,20 +232,32 @@ func (sp *systemProcess) GetMappings() ([]Mapping, uint32, error) {
 	defer mapsFile.Close()
 
 	mappings, numParseErrors, err := parseMappings(mapsFile)
-	if err == nil {
-		fileToMapping := make(map[string]*Mapping, len(mappings))
-		for idx := range mappings {
-			m := &mappings[idx]
-			if m.Inode == 0 {
-				// Ignore mappings that are invalid,
-				// non-existent or are special pseudo-files.
-				continue
-			}
-			fileToMapping[m.Path] = m
-		}
-		sp.fileToMapping = fileToMapping
+	if err != nil {
+		return mappings, numParseErrors, err
 	}
-	return mappings, numParseErrors, err
+
+	if len(mappings) == 0 {
+		// TODO: Test for main thread exit
+		sp.mainThreadExit = true
+		mapsFileAlt, err := os.Open(fmt.Sprintf("/proc/%d/task/%d/maps", sp.pid, sp.tid))
+		if err != nil {
+			return mappings, numParseErrors, err
+		}
+		defer mapsFileAlt.Close()
+
+		mappings, numParseErrors, err = parseMappings(mapsFileAlt)
+		if err != nil {
+			return mappings, numParseErrors, err
+		}
+	}
+
+	fileToMapping := make(map[string]*Mapping, len(mappings))
+	for idx := range mappings {
+		m := &mappings[idx]
+		fileToMapping[m.Path] = m
+	}
+	sp.fileToMapping = fileToMapping
+	return mappings, numParseErrors, nil
 }
 
 func (sp *systemProcess) GetThreads() ([]ThreadInfo, error) {
@@ -271,6 +285,11 @@ func (sp *systemProcess) extractMapping(m *Mapping) (*bytes.Reader, error) {
 func (sp *systemProcess) getMappingFile(m *Mapping) string {
 	if m.IsAnonymous() || m.IsVDSO() {
 		return ""
+	}
+	if sp.mainThreadExit {
+		// Neither /proc/sp.pid/map_files nor /proc/sp.pid/task/sp.tid/map_files
+		// exist if main thread has exited, so we use the mapping path directly.
+		return m.Path
 	}
 	return fmt.Sprintf("/proc/%v/map_files/%x-%x", sp.pid, m.Vaddr, m.Vaddr+m.Length)
 }
