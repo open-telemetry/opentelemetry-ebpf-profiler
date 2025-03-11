a9305571a453961610c8018a5b3a167c9de1b251
Tino Heuberger <tinoheuberger@protonmail.com>
2025-03-11 12:39:38 +0100

    Only read /proc/<pid>/environ if environment variables are requested

--- processmanager/processinfo.go
+++ processmanager/processinfo.go
@@ -91,20 +91,22 @@ func (pm *ProcessManager) updatePidInformation(pid libpf.PID, m *Mapping) (bool,
 		}
 
 		envVarMap := make(map[string]string, len(pm.includeEnvVars))
-		if envVars, err := os.ReadFile(fmt.Sprintf("/proc/%d/environ", pid)); err == nil {
-			// environ has environment variables separated by a null byte (hex: 00)
-			splittedVars := strings.Split(string(envVars), "\000")
-			for _, envVar := range splittedVars {
-				keyValuePair := strings.SplitN(envVar, "=", 2)
+		if len(pm.includeEnvVars) > 0 {
+			if envVars, err := os.ReadFile(fmt.Sprintf("/proc/%d/environ", pid)); err == nil {
+				// environ has environment variables separated by a null byte (hex: 00)
+				splittedVars := strings.Split(string(envVars), "\000")
+				for _, envVar := range splittedVars {
+					keyValuePair := strings.SplitN(envVar, "=", 2)
 
-				// If the entry could not be split at a '=', ignore it
-				// (last entry of environ might be empty)
-				if len(keyValuePair) != 2 {
-					continue
-				}
+					// If the entry could not be split at a '=', ignore it
+					// (last entry of environ might be empty)
+					if len(keyValuePair) != 2 {
+						continue
+					}
 
-				if _, ok := pm.includeEnvVars[keyValuePair[0]]; ok {
-					envVarMap[keyValuePair[0]] = keyValuePair[1]
+					if _, ok := pm.includeEnvVars[keyValuePair[0]]; ok {
+						envVarMap[keyValuePair[0]] = keyValuePair[1]
+					}
 				}
 			}
 		}
