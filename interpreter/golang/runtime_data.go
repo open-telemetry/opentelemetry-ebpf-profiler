cc18855d814874cc4f0d563e6947a4c0b74753e7
Tommy Reilly <gnurizen@gmail.com>
2025-03-26 13:49:37 -0400

    Go version detection tweaks
    To avoid excessive memory usage don't recognize go binaries unless
    the .go.buildinfo section is present.
    
    The removed code came from here:
    
    https://github.com/xyproto/ainur/blob/8c4ac385dd6112d2a7b91e3f59f5e04eacce3553/detect.go#L375
    
    Which came from here:
    
    https://github.com/golang/go/blob/b138f8e4d2912e1105b69b9aaa24c6e8250652c6/src/debug/buildinfo/buildinfo.go#L455
    
    Which is a broken translation of the original code from here:
    
    https://github.com/rsc/goversion/blob/3a30cee7003ec5dc0903682e79b3059c8e101952/version/read.go#L206
    
    Indeed if we look in the first RO data section we can pull the buildinfo
    from go 1.11 and 1.12 binaries.  Ie go version doesn't work on them while
    rsc's goversion does.

--- interpreter/golang/runtime_data.go
+++ interpreter/golang/runtime_data.go
@@ -6,25 +6,9 @@ import "C"
 
 // defaultVersion is used if the go binary has an unrecognized major+minor version.
 // Consider bumping this whenever a new version of Go is released.
-var defaultVersion = "go1.23"
+var latestVersion = "go1.24"
 
 var allOffsets = map[string]C.GoCustomLabelsOffsets{
-	"go1.11": {
-		m_offset:               48,
-		curg:                   192,
-		labels:                 344,
-		hmap_count:             0,
-		hmap_log2_bucket_count: 9,
-		hmap_buckets:           16,
-	},
-	"go1.12": {
-		m_offset:               48,
-		curg:                   192,
-		labels:                 344,
-		hmap_count:             0,
-		hmap_log2_bucket_count: 9,
-		hmap_buckets:           16,
-	},
 	"go1.13": {
 		m_offset:               48,
 		curg:                   192,
