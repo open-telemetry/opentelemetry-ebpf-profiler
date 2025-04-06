2bd6bdd7c01fa43560107262ee2ffb5a43f979aa
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-04-06 16:47:17 +0700

    extract regs state into a new amd package

--- interpreter/python/decode_amd64.go
+++ interpreter/python/decode_amd64.go
@@ -4,45 +4,16 @@
 package python // import "go.opentelemetry.io/ebpf-profiler/interpreter/python"
 
 import (
+	"go.opentelemetry.io/ebpf-profiler/asm/amd"
 	"go.opentelemetry.io/ebpf-profiler/libpf"
 	"golang.org/x/arch/x86/x86asm"
 )
-
-func regIndex(reg x86asm.Reg) int {
-	switch reg {
-	case x86asm.RAX, x86asm.EAX:
-		return 1
-	case x86asm.RBX, x86asm.EBX:
-		return 2
-	case x86asm.RCX, x86asm.ECX:
-		return 3
-	case x86asm.RDX, x86asm.EDX:
-		return 4
-	case x86asm.RDI, x86asm.EDI:
-		return 5
-	case x86asm.RSI, x86asm.ESI:
-		return 6
-	case x86asm.RBP, x86asm.EBP:
-		return 7
-	case x86asm.RSP, x86asm.ESP:
-		return 8
-	case x86asm.RIP:
-		return 9
-	default:
-		return 0
-	}
-}
-
-type regState struct {
-	loadedFrom uint64
-	value      uint64
-}
 
 func decodeStubArgumentAMD64(code []byte, codeAddress, memoryBase uint64) uint64 {
 	targetRegister := x86asm.RDI
 
 	instructionOffset := 0
-	regs := [32]regState{}
+	regs := amd.RegsState{}
 
 	for instructionOffset < len(code) {
 		rem := code[instructionOffset:]
@@ -61,19 +32,18 @@ func decodeStubArgumentAMD64(code []byte, codeAddress, memoryBase uint64) uint64
 		}
 
 		instructionOffset += inst.Len
-		regs[regIndex(x86asm.RIP)].value = codeAddress + uint64(instructionOffset)
+		regs.Set(x86asm.RIP, codeAddress+uint64(instructionOffset), 0)
 
 		if inst.Op == x86asm.CALL || inst.Op == x86asm.JMP {
-			targetRegIdx := regIndex(targetRegister)
-			if regs[targetRegIdx].loadedFrom != 0 {
-				return regs[targetRegIdx].loadedFrom
+			value, loadedFrom := regs.Get(targetRegister)
+			if loadedFrom != 0 {
+				return loadedFrom
 			}
-			return regs[targetRegIdx].value
+			return value
 		}
 
 		if (inst.Op == x86asm.LEA || inst.Op == x86asm.MOV) && inst.Args[0] != nil {
 			if reg, ok := inst.Args[0].(x86asm.Reg); ok {
-				regIdx := regIndex(reg)
 				var value uint64
 				var loadedFrom uint64
 
@@ -81,13 +51,8 @@ func decodeStubArgumentAMD64(code []byte, codeAddress, memoryBase uint64) uint64
 				case x86asm.Imm:
 					value = uint64(src)
 				case x86asm.Mem:
-					baseReg := src.Base
-					baseAddr := regs[regIndex(baseReg)].value
+					baseAddr, _ := regs.Get(src.Base)
 					displacement := uint64(src.Disp)
-
-					if baseReg == x86asm.RIP {
-						baseAddr = codeAddress + uint64(instructionOffset)
-					}
 
 					if inst.Op == x86asm.MOV {
 						value = memoryBase
@@ -96,28 +61,25 @@ func decodeStubArgumentAMD64(code []byte, codeAddress, memoryBase uint64) uint64
 						value = baseAddr + displacement
 					}
 
-					if src.Index != 0 {
-						indexValue := regs[regIndex(src.Index)].value
+					if src.Index != 0 { // todo this is dead code according to test coverage, need a test or remove this
+						indexValue, _ := regs.Get(src.Index)
 						value += indexValue * uint64(src.Scale)
 					}
 
 				case x86asm.Reg:
-					value = regs[regIndex(src)].value
+					value, _ = regs.Get(src)
 				}
 
-				regs[regIdx].value = value
-				regs[regIdx].loadedFrom = loadedFrom
+				regs.Set(reg, value, loadedFrom)
 			}
 		}
 
 		if inst.Op == x86asm.ADD && inst.Args[0] != nil && inst.Args[1] != nil {
-			if reg, ok := inst.Args[0].(x86asm.Reg); ok {
-				if _, ok := inst.Args[1].(x86asm.Mem); ok {
-					regIdx := regIndex(reg)
-					oldValue := regs[regIdx].value
+			if reg, ok0 := inst.Args[0].(x86asm.Reg); ok0 {
+				if _, ok1 := inst.Args[1].(x86asm.Mem); ok1 {
+					oldValue, _ := regs.Get(reg)
 					value := oldValue + memoryBase
-					regs[regIdx].value = value
-					regs[regIdx].loadedFrom = 0
+					regs.Set(reg, value, 0)
 				}
 			}
 		}
