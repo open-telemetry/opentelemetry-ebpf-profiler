4372fc30cc1fb13aeb761526e1d37bb5081ae6dd
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-04-07 20:47:54 +0700

    add endbr64 comment

--- asm/amd/insn.go
+++ asm/amd/insn.go
@@ -3,6 +3,9 @@
 
 package amd // import "go.opentelemetry.io/ebpf-profiler/asm/amd"
 
+// IsEndbr64 returns true if the first 4 bytes of the code is endbr64 instruction
+// https://www.felixcloutier.com/x86/endbr64
+// The second returned argument is the size of the instruction which is always 4
 func IsEndbr64(code []byte) (isEndbr bool, size int) {
 	if len(code) >= 4 &&
 		code[0] == 0xf3 &&
