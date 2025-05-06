e500b4489577a877b588696adf4f792881c86a67
Tommy Reilly <gnurizen@gmail.com>
2025-05-06 09:05:28 -0400

    review fixes

--- interpreter/golabels/golabels.go
+++ interpreter/golabels/golabels.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package golabels // import "go.opentelemetry.io/ebpf-profiler/interpreter/golabels"
 
 import (
--- support/ebpf/util.h
+++ support/ebpf/util.h
@@ -1,7 +1,6 @@
 #ifndef OPTI_UTIL_H
 #define OPTI_UTIL_H
 
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN(a, b) __builtin_elementwise_min(a, b)
 
 #endif
