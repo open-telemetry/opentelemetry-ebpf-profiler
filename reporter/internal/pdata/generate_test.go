8bf70bd0c4c32355b99409c59027d2d4a0b6d932
dmathieu <42@dmathieu.com>
2025-06-02 14:31:52 +0200

    upgrade pdata to 1.7.0

--- reporter/internal/pdata/generate_test.go
+++ reporter/internal/pdata/generate_test.go
@@ -148,17 +148,17 @@ func TestGetDummyMappingIndex(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			fitm := tt.fileIDToMapping
 			stringMap := tt.stringMap
-			profile := pprofile.NewProfile()
-			mgr := samples.NewAttrTableManager(profile.AttributeTable())
+			dic := pprofile.NewProfilesDictionary()
+			mgr := samples.NewAttrTableManager(dic.AttributeTable())
 
-			i := getDummyMappingIndex(fitm, stringMap, mgr, profile, tt.fileID)
+			i := getDummyMappingIndex(fitm, stringMap, mgr, dic, tt.fileID)
 			assert.Equal(t, tt.wantIndex, i)
 			assert.Equal(t, tt.fileIDToMapping, fitm)
 			assert.Equal(t, tt.wantStringMap, stringMap)
 
-			require.Equal(t, len(tt.wantMappingTable), profile.MappingTable().Len())
+			require.Equal(t, len(tt.wantMappingTable), dic.MappingTable().Len())
 			for i, v := range tt.wantMappingTable {
-				mapp := profile.MappingTable().At(i)
+				mapp := dic.MappingTable().At(i)
 				assert.Equal(t, v, mapp.FilenameStrindex())
 			}
 		})
@@ -281,10 +281,10 @@ func TestFunctionTableOrder(t *testing.T) {
 			if expectedProfiles == 0 {
 				return
 			}
-			p := res.ResourceProfiles().At(0).ScopeProfiles().At(0).Profiles().At(0)
-			require.Equal(t, len(tt.wantFunctionTable), p.FunctionTable().Len())
-			for i := 0; i < p.FunctionTable().Len(); i++ {
-				funcName := p.StringTable().At(int(p.FunctionTable().At(i).NameStrindex()))
+			dic := res.ProfilesDictionary()
+			require.Equal(t, len(tt.wantFunctionTable), dic.FunctionTable().Len())
+			for i := 0; i < dic.FunctionTable().Len(); i++ {
+				funcName := dic.StringTable().At(int(dic.FunctionTable().At(i).NameStrindex()))
 				assert.Equal(t, tt.wantFunctionTable[i], funcName)
 			}
 		})
