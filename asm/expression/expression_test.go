1528ec1ede6fa9561598f5faf776ad832694e64f
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2025-06-20 13:32:07 +0700

    remove equalsRecursive

--- asm/expression/expression_test.go
+++ asm/expression/expression_test.go
@@ -4,208 +4,107 @@
 package expression
 
 import (
-	"slices"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestVariable(t *testing.T) {
+func TestExpression(t *testing.T) {
 	t.Run("add sort-summ-immediate", func(t *testing.T) {
 		v := Named("v")
-		assertEqualRecursive(t,
-			Add(v, Imm(14)),
-			Add(Imm(1), Imm(3), Imm(1), v, Imm(9)),
-		)
+		require.Equal(t, Add(v, Imm(14)), Add(Imm(1), Imm(3), Imm(1), v, Imm(9)))
 	})
 
 	t.Run("add 0", func(t *testing.T) {
 		v := Named("v")
-		assertEqualRecursive(t,
-			v,
-			Add(Imm(0), v),
-		)
+		require.Equal(t, v, Add(Imm(0), v))
 	})
 
 	t.Run("add nested", func(t *testing.T) {
 		s1 := Named("s1")
 		s2 := Named("s2")
 		s3 := Named("s3")
-		assertEqualRecursive(t,
-			Add(Add(s1, s3), s2),
-			Add(s1, s3, s2),
-		)
-		assertEqualRecursive(t,
-			Add(Add(s1, s3), s2),
-			Add(s2, s3, s1),
-		)
+		performAssertions := func(e Expression) {
+			opp, ok := e.(*op)
+			require.True(t, ok)
+			require.Len(t, opp.operands, 3)
+			require.Contains(t, opp.operands, s1)
+			require.Contains(t, opp.operands, s2)
+			require.Contains(t, opp.operands, s3)
+		}
+		performAssertions(Add(Add(s1, s3), s2))
+		performAssertions(Add(Add(s1, s3), s2))
 	})
 
 	t.Run("add opt", func(t *testing.T) {
 		v := Named("v")
-		assertEqualRecursive(t,
-			Add(Add(Imm(2), v), Imm(7)),
-			Add(v, Imm(9)),
-		)
+		require.Equal(t, Add(Add(Imm(2), v), Imm(7)), Add(v, Imm(9)))
 	})
 
 	t.Run("add 1 element", func(t *testing.T) {
-		assertEqualRecursive(t,
-			Add(Imm(2)),
-			Imm(2),
-		)
+		require.Equal(t, Add(Imm(2)), Imm(2))
 	})
 
 	t.Run("mul immediate", func(t *testing.T) {
 		v := Named("v")
-		assertEqualRecursive(t,
-			Multiply(v, Imm(27)),
-			Multiply(Imm(1), Imm(3), Imm(1), v, Imm(9)),
-		)
+		require.Equal(t, Multiply(v, Imm(27)), Multiply(Imm(1), Imm(3), Imm(1), v, Imm(9)))
 	})
 
 	t.Run("mul 1", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			v,
-			Multiply(Imm(1), v),
-		)
+		require.Equal(t, v, Multiply(Imm(1), v))
 	})
 
 	t.Run("mul add", func(t *testing.T) {
 		v1 := Named("v1")
 		v2 := Named("v2")
 		v3 := Named("v3")
-		assertEqualRecursive(t,
-			Add(Multiply(v1, v3), Multiply(v2, v3)),
-			Multiply(Add(v1, v2), v3),
-		)
+		require.Equal(t, Add(Multiply(v1, v3), Multiply(v2, v3)), Multiply(Add(v1, v2), v3))
+	})
+
+	t.Run("op order", func(t *testing.T) {
+		v := Named("v")
+		v2 := Mem8(Named("v2"))
+		require.True(t, Multiply(v, v2).Match(Multiply(v2, v)))
 	})
 
 	t.Run("mul order", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			&op{opMul, []Expression{v, Imm(239)}},
-			Multiply(Imm(239), v),
-		)
+		var a Expression = &op{opMul, []Expression{v, Imm(239)}}
+		require.Equal(t, a, Multiply(Imm(239), v))
 	})
 
 	t.Run("mul 0", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			Imm(0),
-			Multiply(Imm(0), Imm(3), Imm(1), v, Imm(9)),
-		)
+		require.Equal(t, Imm(0), Multiply(Imm(0), Imm(3), Imm(1), v, Imm(9)))
 	})
 
 	t.Run("extend nested", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			ZeroExtend(v, 7),
-			ZeroExtend(ZeroExtend(v, 7), 7),
-		)
+		require.Equal(t, ZeroExtend(v, 7), ZeroExtend(ZeroExtend(v, 7), 7))
 	})
 
 	t.Run("extend nested smaller", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			ZeroExtend(v, 5),
-			ZeroExtend(ZeroExtend(v, 7), 5),
-		)
+		require.Equal(t, ZeroExtend(v, 5), ZeroExtend(ZeroExtend(v, 7), 5))
 	})
 	t.Run("extend nested smaller", func(t *testing.T) {
 		v := Named("v")
 
-		assertEqualRecursive(t,
-			ZeroExtend(v, 5),
-			ZeroExtend(ZeroExtend(v, 5), 7),
-		)
+		require.Equal(t, ZeroExtend(v, 5), ZeroExtend(ZeroExtend(v, 5), 7))
 	})
 
 	t.Run("extend 0", func(t *testing.T) {
-		assertEqualRecursive(t,
-			Imm(0),
-			ZeroExtend(Named("v1"), 0),
-		)
+		require.Equal(t, Imm(0), ZeroExtend(Named("v1"), 0))
 	})
 
 	t.Run("nested extend ", func(t *testing.T) {
 		v1 := Named("v1")
-		assertEqualRecursive(t,
-			ZeroExtend(v1, 8),
-			ZeroExtend(ZeroExtend(v1, 8), 8),
-		)
+		require.Equal(t, ZeroExtend(v1, 8), ZeroExtend(ZeroExtend(v1, 8), 8))
 	})
-}
-
-func assertEqualRecursive(t *testing.T, a, b Expression) {
-	if !equalRecursive(a, b) {
-		t.Errorf("expected %s to be recursive equal to %s", a.DebugString(), b.DebugString())
-	}
-}
-
-func equalRecursive(a, b Expression) bool {
-	if ima, aok := a.(*immediate); aok {
-		if imb, bok := b.(*immediate); bok {
-			return ima.Value == imb.Value
-		}
-		return false
-	}
-	if ima, aok := a.(*named); aok {
-		if imb, bok := b.(*named); bok {
-			return ima == imb
-		}
-		return false
-	}
-
-	if ima, aok := a.(*ImmediateCapture); aok {
-		if imb, bok := b.(*ImmediateCapture); bok {
-			return ima == imb
-		}
-		return false
-	}
-	if ima, aok := a.(*mem); aok {
-		if imb, bok := b.(*mem); bok {
-			return ima.segment == imb.segment && equalRecursive(ima.at, imb.at)
-		}
-		return false
-	}
-	if ima, aok := a.(*extend); aok {
-		if imb, bok := b.(*extend); bok {
-			return ima.bits == imb.bits && equalRecursive(ima.v, imb.v)
-		}
-		return false
-	}
-	if ima, aok := a.(*op); aok {
-		if imb, bok := b.(*op); bok {
-			return ima.typ == imb.typ && equalOperands(ima.operands, imb.operands)
-		}
-		return false
-	}
-	return false
-}
-
-func equalOperands(a, b operands) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	acopy := make(operands, len(a))
-	copy(acopy, a)
-	for _, it := range b {
-		eq := false
-		for j, jit := range acopy {
-			if equalRecursive(it, jit) {
-				eq = true
-				acopy = slices.Delete(acopy, j, j+1)
-				break
-			}
-		}
-		if !eq {
-			return false
-		}
-	}
-	return len(acopy) == 0
 }
