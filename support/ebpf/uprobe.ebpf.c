09323070457f8af71274abb0aa7997f3729ffa48
Florian Lehner <florian.lehner@elastic.co>
2025-07-23 14:04:43 +0200

    fixup: reuse kprobes as uprobes
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- support/ebpf/uprobe.ebpf.c
+++ support/ebpf/uprobe.ebpf.c
@@ -2,23 +2,6 @@
 #include "tracemgmt.h"
 #include "types.h"
 
-// uprobe_progs maps from a program ID to a uprobe eBPF program
-bpf_map_def SEC("maps") uprobe_progs = {
-  .type        = BPF_MAP_TYPE_PROG_ARRAY,
-  .key_size    = sizeof(u32),
-  .value_size  = sizeof(u32),
-  .max_entries = NUM_TRACER_PROGS,
-};
-
-// uprobe__dummy is never loaded or called. It just makes sure uprobe_progs is
-// referenced and make the compiler and linker happy.
-SEC("uprobe/dummy")
-int uprobe__dummy(struct pt_regs *ctx)
-{
-  bpf_tail_call(ctx, &uprobe_progs, 0);
-  return 0;
-}
-
 // uprobe__generic serves as entry point for uprobe based profiling.
 SEC("uprobe/generic")
 int uprobe__generic(void *ctx)
