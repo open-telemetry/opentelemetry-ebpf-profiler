4712b8055763be853e02bcc979e05bcb6c2ca746
Florian Lehner <florian.lehner@elastic.co>
2025-07-31 10:30:20 +0200

    Go: refactor testutils
    Reduce supplementary test* packages by refactoring code.
    
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- tracer/tracer_test.go
+++ tracer/tracer_test.go
@@ -1,10 +1,11 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Package tracer contains functionality for populating tracers.
 package tracer // import "go.opentelemetry.io/ebpf-profiler/tracer"
 
-import cebpf "github.com/cilium/ebpf"
+import (
+	cebpf "github.com/cilium/ebpf"
+)
 
 // Make accessible for testing
 func (t *Tracer) GetEbpfMaps() map[string]*cebpf.Map {
