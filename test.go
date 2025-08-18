d877b1bba9ad4f05d07592049abfb9bda500c9e4
Dale Hamel <dale.hamel@shopify.com>
2025-08-18 13:21:17 -0400

    Fix up test program

--- test.go
+++ test.go
@@ -7,28 +7,28 @@ import (
 	"fmt"
 )
 
-// /usr/bin/time -v go run test.go /home/dalehamel.linux/.rubies/ruby-3.4.4/bin/ruby rb_execution_context_struct rb_control_frame_struct rb_iseq_struct rb_iseq_constant_body rb_iseq_location_struct iseq_insn_info_entry RString RArray succ_dict_block succ_index_table
+// /usr/bin/time -v go run test.go /home/dalehamel.linux/.rubies/ruby-3.4.4/bin/ruby rb_execution_context_type rb_control_frame_type rb_iseq_type rb_iseq_constant_body rb_iseq_location_type iseq_insn_info_entry RString RArray succ_dict_block succ_index_table
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <elf-file> [struct-name]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s <elf-file> [type-name]\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	elfFile := os.Args[1]
-	specificStructs := []string{}
+	specificTypes := []string{}
 	if len(os.Args) > 2 {
-		specificStructs = os.Args[2:]
+		specificTypes = os.Args[2:]
 	}
 
 	pf, err := pfelf.Open(elfFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening ELF file for %s: %v\n", specificStructs, err)
+		fmt.Fprintf(os.Stderr, "Error opening ELF file for %s: %v\n", specificTypes, err)
 		os.Exit(1)
 	}
 	defer pf.Close()
 
-	data, err := pf.StructData(specificStructs)
+	data, err := pf.TypeData(specificTypes)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening ELF file: %v\n", err)
 		os.Exit(1)
