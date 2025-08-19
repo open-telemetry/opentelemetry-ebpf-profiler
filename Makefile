e63fc80ab7b67c7beae9478f2e92fac79411afb3
Florian Lehner <florian.lehner@elastic.co>
2025-08-19 09:55:53 +0200

    refactor pprof test for golables
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- Makefile
+++ Makefile
@@ -120,19 +120,9 @@ test-deps:
 		($(MAKE) -C "$(testdata_dir)") || exit ; \
 	)
 
-TEST_INTEGRATION_BINARY_DIRS := tracer processmanager/ebpf support
+TEST_INTEGRATION_BINARY_DIRS := tracer processmanager/ebpf support interpreter/golabels/integrationtests
 
-# These binaries are named ".test" to get included into bluebox initramfs
-support/golbls_current.test: generate ebpf
-	CGO_ENABLED=0 GOTOOLCHAIN=go1.24.6 go test ./interpreter/golabels/integrationtests -c -trimpath -tags $(GO_TAGS),nocgo,integration  -o $@
-
-support/golbls_next.test: generate ebpf
-	CGO_ENABLED=0 GOTOOLCHAIN=go1.25.0 go test ./interpreter/golabels/integrationtests -c -trimpath -tags $(GO_TAGS),nocgo,integration  -o $@
-
-support/golbls_next_cgo.test: generate ebpf
-	CGO_ENABLED=1 GOTOOLCHAIN=go1.25.0 go test ./interpreter/golabels/integrationtests -c -ldflags '-extldflags "-static"' -trimpath -tags $(GO_TAGS),withcgo,integration  -o $@
-
-integration-test-binaries: support/golbls_current.test support/golbls_next.test support/golbls_next_cgo.test
+integration-test-binaries: generate ebpf
 	$(foreach test_name, $(TEST_INTEGRATION_BINARY_DIRS), \
 		(go test -ldflags='-extldflags=-static' -trimpath -c \
 			-tags $(GO_TAGS),static_build,integration \
--- interpreter/golabels/integrationtests/golabels_test.go
+++ interpreter/golabels/integrationtests/golabels_test.go
@@ -1,17 +1,17 @@
-//go:build integration
-
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
+
+//go:generate make pprof
 
 package integrationtests
 
 import (
+	_ "embed"
+
 	"context"
 	"math"
-	"math/rand"
 	"os"
-	"runtime/debug"
-	"runtime/pprof"
+	"os/exec"
 	"strings"
 	"testing"
 	"time"
@@ -22,6 +22,17 @@ import (
 	"go.opentelemetry.io/ebpf-profiler/reporter"
 	"go.opentelemetry.io/ebpf-profiler/tracer"
 	tracertypes "go.opentelemetry.io/ebpf-profiler/tracer/types"
+)
+
+var (
+	//go:embed pprof_1_23
+	pprof_1_23 []byte
+
+	//go:embed pprof_1_24
+	pprof_1_24 []byte
+
+	//go:embed pprof_1_24_cgo
+	pprof_1_24_cgo []byte
 )
 
 type mockIntervals struct{}
@@ -39,120 +50,107 @@ func isRoot() bool {
 	return os.Geteuid() == 0
 }
 
-//nolint:gosec
-func randomString(n int) string {
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	s := make([]rune, n)
-	for i := range s {
-		s[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(s)
-}
-
-func setPprofLabels(t *testing.T, ctx context.Context, cookie string, busyFunc func()) {
-	t.Helper()
-	labels := pprof.Labels(
-		"l1"+cookie, "label1"+randomString(16),
-		"l2"+cookie, "label2"+randomString(24),
-		"l3"+cookie, "label3"+randomString(48))
-	lastUpdate := time.Now()
-	pprof.Do(t.Context(), labels, func(context.Context) {
-		for time.Since(lastUpdate) < 10*time.Second {
-			// CPU go burr on purpose.
-			busyFunc()
-			if ctx.Err() != nil {
-				return
-			}
-		}
-	})
-}
-
 func Test_Golabels(t *testing.T) {
 	if !isRoot() {
 		t.Skip("root privileges required")
 	}
 
-	buildInfo, ok := debug.ReadBuildInfo()
-	if !ok {
-		t.Fatalf("Failed to get build info")
+	tests := map[string]struct {
+		bin []byte
+	}{
+		"pprof_1_23":     {bin: pprof_1_23},
+		"pprof_1_24":     {bin: pprof_1_24},
+		"pprof_1_24_cgo": {bin: pprof_1_24_cgo},
 	}
 
-	withCGO := false
-	for _, setting := range buildInfo.Settings {
-		if setting.Key == "CGO_ENABLED" {
-			withCGO = true
-		}
-	}
-	t.Logf("CGo is enabled: %t", withCGO)
-
-	cookie := buildInfo.GoVersion
-
-	t.Run(cookie, func(t *testing.T) {
-		ctx, cancel := context.WithCancel(t.Context())
-		defer cancel()
-
-		enabledTracers, _ := tracertypes.Parse("")
-		enabledTracers.Enable(tracertypes.Labels)
-
-		trc, err := tracer.NewTracer(ctx, &tracer.Config{
-			Reporter:               &mockReporter{},
-			Intervals:              &mockIntervals{},
-			IncludeTracers:         enabledTracers,
-			SamplesPerSecond:       20,
-			ProbabilisticInterval:  100,
-			ProbabilisticThreshold: 100,
-			OffCPUThreshold:        uint32(math.MaxUint32 / 100),
-			VerboseMode:            true,
-		})
-		require.NoError(t, err)
-
-		trc.StartPIDEventProcessor(ctx)
-
-		err = trc.AttachTracer()
-		require.NoError(t, err)
-
-		t.Log("Attached tracer program")
-
-		err = trc.EnableProfiling()
-		require.NoError(t, err)
-
-		err = trc.AttachSchedMonitor()
-		require.NoError(t, err)
-
-		traceCh := make(chan *host.Trace)
-
-		err = trc.StartMapMonitors(ctx, traceCh)
-		require.NoError(t, err)
-
-		go setPprofLabels(t, ctx, cookie, busyFunc)
-
-		for trace := range traceCh {
-			if trace == nil {
-				continue
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			exe, err := os.CreateTemp(t.TempDir(), name)
+			if err != nil {
+				t.Fatal(err)
 			}
-			if len(trace.CustomLabels) > 0 {
-				hits := 0
-				for k, v := range trace.CustomLabels {
-					switch k {
-					case "l1" + cookie:
-						require.Len(t, v, 22)
-						require.True(t, strings.HasPrefix(v, "label1"))
-						hits |= (1 << 0)
-					case "l2" + cookie:
-						require.Len(t, v, 30)
-						require.True(t, strings.HasPrefix(v, "label2"))
-						hits |= (1 << 1)
-					case "l3" + cookie:
-						require.Len(t, v, 47)
-						require.True(t, strings.HasPrefix(v, "label3"))
-						hits |= (1 << 2)
+			defer os.Remove(exe.Name())
+
+			if _, err = exe.Write(tc.bin); err != nil {
+				t.Fatal(err)
+			}
+			if err = exe.Close(); err != nil {
+				t.Fatal(err)
+			}
+
+			if err = os.Chmod(exe.Name(), 0o755); err != nil {
+				t.Fatal(err)
+			}
+
+			ctx, cancel := context.WithCancel(t.Context())
+			defer cancel()
+
+			enabledTracers, _ := tracertypes.Parse("")
+			enabledTracers.Enable(tracertypes.Labels)
+
+			trc, err := tracer.NewTracer(ctx, &tracer.Config{
+				Reporter:               &mockReporter{},
+				Intervals:              &mockIntervals{},
+				IncludeTracers:         enabledTracers,
+				SamplesPerSecond:       20,
+				ProbabilisticInterval:  100,
+				ProbabilisticThreshold: 100,
+				OffCPUThreshold:        uint32(math.MaxUint32 / 100),
+				VerboseMode:            true,
+			})
+			require.NoError(t, err)
+
+			trc.StartPIDEventProcessor(ctx)
+
+			err = trc.AttachTracer()
+			require.NoError(t, err)
+
+			t.Log("Attached tracer program")
+
+			err = trc.EnableProfiling()
+			require.NoError(t, err)
+
+			err = trc.AttachSchedMonitor()
+			require.NoError(t, err)
+
+			traceCh := make(chan *host.Trace)
+
+			err = trc.StartMapMonitors(ctx, traceCh)
+			require.NoError(t, err)
+
+			go func() {
+				if err := exec.CommandContext(ctx, exe.Name()).Run(); err != nil {
+					t.Log(err)
+				}
+			}()
+
+			for trace := range traceCh {
+				if trace == nil {
+					continue
+				}
+				if len(trace.CustomLabels) > 0 {
+					hits := 0
+					for k, v := range trace.CustomLabels {
+						if strings.HasPrefix(k, "l1") {
+							require.Len(t, v, 22)
+							require.True(t, strings.HasPrefix(v, "label1"))
+							hits |= (1 << 0)
+						} else if strings.HasPrefix(k, "l2") {
+							require.Len(t, v, 30)
+							require.True(t, strings.HasPrefix(v, "label2"))
+							hits |= (1 << 1)
+						} else if strings.HasPrefix(k, "l3") {
+							require.Len(t, v, 47)
+							require.True(t, strings.HasPrefix(v, "label3"))
+							hits |= (1 << 2)
+						}
+					}
+					if hits == (1<<0 | 1<<1 | 1<<2) {
+						cancel()
+						break
 					}
 				}
-				if hits == (1<<0 | 1<<1 | 1<<2) {
-					cancel()
-					break
-				}
 			}
-		}
-	})
+		})
+	}
 }
