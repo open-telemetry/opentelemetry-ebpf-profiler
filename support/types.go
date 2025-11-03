cb8e9ef55dd14d4d4b6fe923e5b0280d85acf2a9
Dale Hamel <dale.hamel@shopify.com>
2025-11-03 17:15:54 -0500

    Handle JIT frames

--- support/types.go
+++ support/types.go
@@ -281,6 +281,8 @@ type RubyProcInfo struct {
 	Tls_module_id                uint32
 	Current_ctx_ptr              uint64
 	Has_objspace                 bool
+	Jit_start                    uint64
+	Jit_end                      uint64
 	Vm_stack                     uint8
 	Vm_stack_size                uint8
 	Cfp                          uint8
@@ -394,6 +396,7 @@ const (
 	RubyFrameTypeCmeCfunc = 0x2
 	RubyFrameTypeIseq     = 0x3
 	RubyFrameTypeGc       = 0x4
+	RubyFrameTypeJit      = 0x5
 )
 
 var MetricsTranslation = []metrics.MetricID{
