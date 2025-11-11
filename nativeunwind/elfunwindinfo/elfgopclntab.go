40664e605831a3cc6a88a341f0b87172d30a694f
Roger Coll <roger.coll@elastic.co>
2025-11-11 08:13:26 +0100

    fix: do not incorrectly symbolize addresses beyong gopclntab end (#950)
    Co-authored-by: Timo Teräs <timo.teras@iki.fi>

--- nativeunwind/elfunwindinfo/elfgopclntab.go
+++ nativeunwind/elfunwindinfo/elfgopclntab.go
@@ -541,11 +541,17 @@ func (g *Gopclntab) mapPcval(offs int32, startPc, pc uint) (int32, bool) {
 
 // Symbolize returns the file, line and function information for given PC
 func (g *Gopclntab) Symbolize(pc uintptr) (sourceFile string, line uint, funcName string) {
-	index := sort.Search(g.numFuncs, func(i int) bool {
+	// Binary search for the matching go function maps entry. The search
+	// lambda makes 'sort.Search' return the first entry that is larger
+	// than the pc. Thus -1 is needed to get index for the first entry
+	// which is equal or less than pc. The gopclntab has an extra entry in
+	// the end to indicate the end of Go code, use that to determine
+	// if the pc is higher than any Go function address.
+	index := sort.Search(g.numFuncs+1, func(i int) bool {
 		funcPc, _ := g.getFuncMapEntry(i)
 		return funcPc > pc
 	}) - 1
-	if index < 0 {
+	if index >= g.numFuncs || index < 0 {
 		return "", 0, ""
 	}
 
