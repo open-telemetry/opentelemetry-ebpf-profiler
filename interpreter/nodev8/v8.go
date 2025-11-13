5efb8285a4c4de5244671d7f8826b16e90481fbf
Timo Teräs <timo.teras@iki.fi>
2025-11-13 13:59:55 +0200

    Update interpreter/nodev8/v8.go
    Co-authored-by: Christos Kalkanis <christos.kalkanis@elastic.co>

--- interpreter/nodev8/v8.go
+++ interpreter/nodev8/v8.go
@@ -2117,7 +2117,7 @@ func locateSnapshotArea(ef *pfelf.File) util.Range {
 		if err != nil {
 			return util.Range{}
 		}
-		// Check that there is a a large gap.
+		// Check that there is a large gap.
 		if fde.PCBegin - prevEnd > 512 * 1024 {
 			log.Debugf("located snapshot area: %#x - %#x", prevEnd, fde.PCBegin)
 			return util.Range{
