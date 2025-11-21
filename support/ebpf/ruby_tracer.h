d8e7c9ecfd34a79237faa49f9c77d9821236e4ac
Dale Hamel <dale.hamel@shopify.com>
2025-11-21 15:27:58 -0500

    Fix lineno decoding by passing cfp->iseq

--- support/ebpf/ruby_tracer.h
+++ support/ebpf/ruby_tracer.h
@@ -1,5 +1,5 @@
 #define ADDR_MASK_48_BIT 0x0000FFFFFFFFFFFFULL // Lower 48 bits for address
-#define EXTRA_TYPE_MASK  0x00FF000000000000ULL // Bits 48-55 for uint8
+#define EXTRA_TYPE_MASK  0x000F000000000000ULL // Bits 49-52 for Frame type
 
 #define FRAME_TYPE_NONE      0
 #define FRAME_TYPE_CME_ISEQ  1
