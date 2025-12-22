092e9853724d7b45a046c4ad7e0d6df2f7224a1c
Florian Lehner <florian.lehner@elastic.co>
2025-12-22 16:24:11 +0100

    apply feedback
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- nativeunwind/elfunwindinfo/elfgopclntab.go
+++ nativeunwind/elfunwindinfo/elfgopclntab.go
@@ -469,7 +469,11 @@ func NewGopclntab(ef *pfelf.File) (*Gopclntab, error) {
 		g.funcdata = g.functab
 		g.textStart = hdr118.textStart
 		if g.textStart == 0 {
-			// Starting with Go 1.26 the field textStart is set to 0.
+			// Starting with Go 1.26 the field textStart is set to 0 but moduledata.text
+			// which contains the same value is unaffected.
+			// The following logic assumes that .text matches moduledata.text
+			// which might not always be true (in which case we need to switch to moduledata.text
+			// which can be found through runtime.firstmoduledata).
 			//
 			//nolint:lll
 			// See https://github.com/golang/go/commit/0e1bd8b5f17e337df0ffb57af03419b96c695fe4
