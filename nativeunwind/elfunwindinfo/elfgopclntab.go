3ee0fcfc3d6184ccf4780e9311e2f59d2b017961
Florian Lehner <florian.lehner@elastic.co>
2025-12-22 08:29:57 +0100

    gopclntab: Use address of .text section for Go 1.26
    Starting with Go 1.26 the field textStart in the gopclntab header is set to 0.
    Use the address of the .text section instead.
    
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- nativeunwind/elfunwindinfo/elfgopclntab.go
+++ nativeunwind/elfunwindinfo/elfgopclntab.go
@@ -468,6 +468,18 @@ func NewGopclntab(ef *pfelf.File) (*Gopclntab, error) {
 		g.functab = data[hdr118.pclnOffset:]
 		g.funcdata = g.functab
 		g.textStart = hdr118.textStart
+		if g.textStart == 0 {
+			// Starting with Go 1.26 the field textStart is set to 0.
+			//
+			//nolint:lll
+			// See https://github.com/golang/go/commit/0e1bd8b5f17e337df0ffb57af03419b96c695fe4
+			for _, sec := range ef.Sections {
+				if sec.Name == ".text" {
+					g.textStart = uintptr(sec.Addr)
+					break
+				}
+			}
+		}
 		// With the change of the type of the first field of _func in Go 1.18, this
 		// value is now hard coded.
 		//
