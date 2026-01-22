open-telemetry/opentelemetry-ebpf-profiler

commit c96e12686646b106e9cc88a42d7551018062250c

BPF: dotnet: make nibble map reading more compliant (#142)
Remove the non-obvious reading past end-of-array to next member,
and add modulo arithmetic to ensure that bpf verifier is happy.

fixes #141

support/ebpf/dotnet_tracer.ebpf.c
@@ -53,7 +53,7 @@ ErrorCode dotnet_find_code_start(PerCPURecord *record, DotnetProcInfo *vi, u64 p
   //   text_section_id   = pHp->pHdrMap (pointer to the nibble map)
   const UnwindState *state = &record->state;
   DotnetUnwindScratchSpace *scratch = &record->dotnetUnwindScratch;
-  const int map_elements = sizeof(scratch->map)/sizeof(scratch->map[0]);
+  const int map_elements = sizeof(scratch->map)/sizeof(scratch->map[0])/2;
   u64 pc_base = state->text_section_bias;
   u64 pc_delta = pc - pc_base;
   u64 map_start = state->text_section_id;
@@ -71,7 +71,8 @@ ErrorCode dotnet_find_code_start(PerCPURecord *record, DotnetProcInfo *vi, u64 p
     // We can read full scratch buffer, adjust map_start so that last entry read corresponds pc_delta
     map_start += pc_delta/DOTNET_CODE_BYTES_PER_ENTRY*sizeof(u32) - sizeof(scratch->map) + sizeof(u32);
   }
-  if (bpf_probe_read_user(&scratch->map[offs], sizeof(scratch->map), (void*) map_start)) {
+  offs %= map_elements;
+  if (bpf_probe_read_user(&scratch->map[offs], sizeof(scratch->map)/2, (void*) map_start)) {
     goto bad_code_header;
   }
 

support/ebpf/types.h
@@ -615,9 +615,9 @@ typedef struct DotnetUnwindScratchSpace {
   // Buffer to read nibble map to locate code start. One map entry allows seeking backwards
   // 32*8 = 256 bytes of code. This defines the maximum size for a JITted function we
   // can recognize: 256 bytes/element * 128 elements = 32kB function size.
-  u32 map[128];
-  // Extra space to read to map fixed amount of bytes, but to dynamic offset.
-  u32 extra[128];
+  // Multiplied by two for extra space to read to this array a fixed amount of bytes
+  // to a dynamic offset.
+  u32 map[2*128];
 } DotnetUnwindScratchSpace;
 
 // Container for additional scratch space needed by the HotSpot unwinder.

commit 04b6dec5a782d2679eaea3915092df566768dc51

interpreter/dotnet: support dotnet9 and fixes
- add dotnet9 introspection data, and coredump test
- fixup unwinder code start finding (regression
  since commit c96e12686646b106e9cc88a42d7551018062250c
  "BPF: dotnet: make nibble map reading more compliant (#142)"
- fixup method index lookup binary search to find
  the last matching methodIdx entry
- fixup typo in extraDebugInfoPatchPoint

interpreter/dotnet/data.go
@@ -204,7 +204,7 @@ func (d *dotnetData) loadIntrospectionData() {
 
 	// Version specific introspection data
 	switch d.version >> 24 {
-	case 6:
+	case 6, 7:
 		vms.DacTable.DelegateInvokeStubManager = 0xe
 		vms.DacTable.VirtualCallStubManagerManager = 0xf
 		vms.RangeSection.Next = 0x18
@@ -212,19 +212,29 @@ func (d *dotnetData) loadIntrospectionData() {
 		vms.RangeSection.HeapList = 0x30
 		vms.RangeSection.Module = 0x30
 		vms.RangeSection.SizeOf = 0x38
+		d.walkRangeSectionsMethod = (*dotnetInstance).walkRangeSectionList
+	case 8, 9:
+		vms.DacTable.VirtualCallStubManagerManager = 0xe
+		vms.RangeSection.Flags = 0x10
+		vms.RangeSection.Module = 0x20
+		vms.RangeSection.HeapList = 0x28
+		vms.RangeSection.RangeList = 0x30
+		vms.RangeSection.SizeOf = 0x38
+		vms.CodeRangeMapRangeList.RangeListType = 0x120
+		vms.MethodDesc.TokenRemainderBits = 12
+		vms.PatchpointInfo.SizeOf = 32
+		vms.PatchpointInfo.NumberOfLocals = 8
+		vms.Module.SimpleName = 0x108
+		d.walkRangeSectionsMethod = (*dotnetInstance).walkRangeSectionMap
+	}
+
+	switch d.version >> 24 {
+	case 6:
 		vms.MethodDesc.TokenRemainderBits = 14
 		vms.Module.SimpleName = 0x8
 		vms.PatchpointInfo.SizeOf = 20
 		vms.PatchpointInfo.NumberOfLocals = 0
-		d.walkRangeSectionsMethod = (*dotnetInstance).walkRangeSectionList
 	case 7:
-		vms.DacTable.DelegateInvokeStubManager = 0xe
-		vms.DacTable.VirtualCallStubManagerManager = 0xf
-		vms.RangeSection.Next = 0x18
-		vms.RangeSection.Flags = 0x28
-		vms.RangeSection.HeapList = 0x30
-		vms.RangeSection.Module = 0x30
-		vms.RangeSection.SizeOf = 0x38
 		vms.MethodDesc.TokenRemainderBits = 13
 		// Module inherits from ModuleBase with quite a bit of data
 		// see: https://github.com/dotnet/runtime/pull/71271
@@ -241,21 +251,10 @@ func (d *dotnetData) loadIntrospectionData() {
 		vms.PrecodeStubManager.FixupPrecodeRangeList = vms.StubManager.SizeOf +
 			vms.LockedRangeList.SizeOf
 		vms.VirtualCallStubManager.Next = 0x6e8
-		d.walkRangeSectionsMethod = (*dotnetInstance).walkRangeSectionList
 	case 8:
-		vms.DacTable.VirtualCallStubManagerManager = 0xe
-		vms.RangeSection.Flags = 0x10
-		vms.RangeSection.Module = 0x20
-		vms.RangeSection.HeapList = 0x28
-		vms.RangeSection.RangeList = 0x30
-		vms.RangeSection.SizeOf = 0x38
-		vms.CodeRangeMapRangeList.RangeListType = 0x120
-		vms.MethodDesc.TokenRemainderBits = 12
-		vms.Module.SimpleName = 0x108
-		vms.PatchpointInfo.SizeOf = 32
-		vms.PatchpointInfo.NumberOfLocals = 8
 		vms.VirtualCallStubManager.Next = 0x268
-		d.walkRangeSectionsMethod = (*dotnetInstance).walkRangeSectionMap
+	case 9:
+		vms.VirtualCallStubManager.Next = 0x260
 	}
 
 	// Calculated masks

interpreter/dotnet/dotnet.go
@@ -149,7 +149,7 @@ func Loader(_ interpreter.EbpfHandler, info *interpreter.LoaderInfo) (interprete
 	version := dotnetVer(uint32(major), uint32(minor), uint32(release))
 
 	// dotnet8 requires additional support for RangeSectionMap and MethodDesc updates
-	if version < dotnetVer(6, 0, 0) || version >= dotnetVer(9, 0, 0) {
+	if version < dotnetVer(6, 0, 0) || version >= dotnetVer(10, 0, 0) {
 		return nil, fmt.Errorf("dotnet version %d.%d.%d not supported",
 			major, minor, release)
 	}

interpreter/dotnet/method.go
@@ -40,7 +40,7 @@ const (
 
 	// CLR internal debug info flags
 	// https://github.com/dotnet/runtime/blob/v7.0.15/src/coreclr/vm/debuginfostore.cpp#L458
-	extraDebugInfoPathcPoint = 0x01
+	extraDebugInfoPatchPoint = 0x01
 	extraDebugInfoRich       = 0x02
 )
 
@@ -164,10 +164,10 @@ func (m *dotnetMethod) readDebugInfo(r *cachingReader, d *dotnetData) error {
 	if err != nil {
 		return fmt.Errorf("failed to read flags: %w", err)
 	}
-	if flags&^(extraDebugInfoPathcPoint|extraDebugInfoRich) != 0 {
+	if flags&^(extraDebugInfoPatchPoint|extraDebugInfoRich) != 0 {
 		return fmt.Errorf("flags (%#x) not supported", flags)
 	}
-	if flags&extraDebugInfoPathcPoint != 0 {
+	if flags&extraDebugInfoPatchPoint != 0 {
 		// skip PatchpointInfo
 		// https://github.com/dotnet/runtime/blob/main/src/coreclr/vm/debuginfostore.cpp#L741-L746
 		// https://github.com/dotnet/runtime/blob/v7.0.15/src/coreclr/inc/patchpointinfo.h#L29-L35

interpreter/dotnet/pe.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"path"
 	"slices"
+	"sort"
 	"sync/atomic"
 	"time"
 
@@ -1135,23 +1136,12 @@ func (pp *peParser) parse() error {
 }
 
 func (pi *peInfo) resolveMethodName(methodIdx uint32) libpf.String {
-	if methodIdx == 0 || methodIdx > uint32(len(pi.methodSpecs)) {
+	idx := sort.Search(len(pi.typeSpecs), func(idx int) bool {
+		return pi.typeSpecs[idx].methodIdx > methodIdx
+	}) - 1
+	if methodIdx == 0 || methodIdx > uint32(len(pi.methodSpecs)) || idx < 0 {
 		return libpf.Intern(fmt.Sprintf("<invalid method index %d/%d>",
 			methodIdx, len(pi.methodSpecs)))
-	}
-
-	idx, ok := slices.BinarySearchFunc(pi.typeSpecs, methodIdx,
-		func(typespec peTypeSpec, methodIdx uint32) int {
-			if methodIdx < typespec.methodIdx {
-				return 1
-			}
-			if methodIdx > typespec.methodIdx {
-				return -1
-			}
-			return 0
-		})
-	if !ok {
-		idx--
 	}
 
 	typeSpec := &pi.typeSpecs[idx]

support/ebpf/dotnet_tracer.ebpf.c
@@ -73,7 +73,7 @@ static EBPF_INLINE ErrorCode dotnet_find_code_start(PerCPURecord *record, u64 pc
     // We can read full scratch buffer, adjust map_start so that last entry read corresponds
     // pc_delta
     map_start +=
-      pc_delta / DOTNET_CODE_BYTES_PER_ENTRY * sizeof(u32) - sizeof(scratch->map) + sizeof(u32);
+      pc_delta / DOTNET_CODE_BYTES_PER_ENTRY * sizeof(u32) - sizeof(scratch->map) / 2 + sizeof(u32);
     offs = 0;
   }
   offs %= map_elements;

tools/coredump/testsources/dotnet/helloworld/helloworld.csproj
@@ -2,7 +2,7 @@
 
   <PropertyGroup>
     <OutputType>Exe</OutputType>
-    <TargetFrameworks>net6.0;net7.0;net8.0</TargetFrameworks>
+    <TargetFrameworks>net6.0;net7.0;net8.0;net9.0</TargetFrameworks>
     <ImplicitUsings>enable</ImplicitUsings>
     <Nullable>enable</Nullable>
   </PropertyGroup>