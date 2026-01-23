4c0fddd96bd4406164250a03d9146677101d0545
Roger Coll <rogercoll@protonmail.com>
2026-01-23 10:19:13 +0100

    fix codespell

--- reporter/internal/pdata/generate_test.go
+++ reporter/internal/pdata/generate_test.go
@@ -89,8 +89,8 @@ func TestGetDummyMappingIndex(t *testing.T) {
 
 			require.Equal(t, len(tt.wantMappingTable), dic.MappingTable().Len())
 			for i, v := range tt.wantMappingTable {
-				mapp := dic.MappingTable().At(i)
-				assert.Equal(t, v, mapp.FilenameStrindex())
+				mappingTable := dic.MappingTable().At(i)
+				assert.Equal(t, v, mappingTable.FilenameStrindex())
 			}
 		})
 	}
