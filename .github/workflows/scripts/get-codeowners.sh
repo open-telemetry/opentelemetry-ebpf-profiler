38272c21332314dedb0c56fdadd0b8bf8da2f86c
Roger Coll <rogercoll@protonmail.com>
2026-01-26 16:04:19 +0100

    ci: add scripts to request review using gh cli

---
+++ .github/workflows/scripts/get-codeowners.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+#
+# This script checks the GitHub CODEOWNERS file for any code owners
+# of profiler interpreters and returns a string of the code owners if it
+# finds them.
+
+set -euo pipefail
+
+get_codeowners() {
+  # grep arguments explained:
+  #   -m 1: Match the first occurrence
+  #   ^: Match from the beginning of the line
+  #   ${1}: Insert first argument given to this function
+  #   [\/]\?: Match 0 or 1 instances of a forward slash
+  #   \s: Match any whitespace character
+(grep -m 1 "^${1}[\/]\?\s" .github/CODEOWNERS || true) | \
+        sed 's/   */ /g' | \
+        cut -f3- -d ' '
+}
+
+if [[ -z "${INTERPRETER:-}" ]]; then
+    echo "INTERPRETER has not been set, please ensure it is set."
+    exit 1
+fi
+
+OWNERS="$(get_codeowners "${INTERPRETER}")"
+
+echo "${OWNERS}"
