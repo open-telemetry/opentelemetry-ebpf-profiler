62b4e1ce2ed8cd83fda9daea0de43972079f09c5
NimrodAvni78 <nimrod@coralogix.com>
2026-02-02 17:26:18 +0200

    fix metric const generation

--- metrics/genids/main.go
+++ metrics/genids/main.go
@@ -14,6 +14,7 @@ type metricDef struct {
 	Description string `json:"description"`
 	MetricType  string `json:"type"`
 	Name        string `json:"name"`
+	Const       string `json:"const"`
 	FieldName   string `json:"field"`
 	ID          uint32 `json:"id"`
 	Obsolete    bool   `json:"obsolete"`
@@ -56,7 +57,7 @@ func main() {
 
 		output.WriteString(
 			fmt.Sprintf("\n\t// %s\n\tID%s = %d\n",
-				m.Description, m.Name, m.ID))
+				m.Description, m.Const, m.ID))
 	}
 
 	output.WriteString(
