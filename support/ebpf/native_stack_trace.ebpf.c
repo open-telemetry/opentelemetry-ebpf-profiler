f1c902b078567f65860c12fcea33f010722fdf72
Brennan Vincent <brennan@umanwizard.com>
2026-02-17 15:45:49 -0700

    bump frame and unwind limits

--- support/ebpf/native_stack_trace.ebpf.c
+++ support/ebpf/native_stack_trace.ebpf.c
@@ -75,7 +75,7 @@ struct unwind_info_array_t {
 } unwind_info_array SEC(".maps");
 
 // The number of native frames to unwind per frame-unwinding eBPF program.
-#define NATIVE_FRAMES_PER_PROGRAM 5
+#define NATIVE_FRAMES_PER_PROGRAM 8
 
 // The decision whether to unwind native stacks or interpreter stacks is made by checking if a given
 // PC address falls into the "interpreter loop" of an interpreter. This map helps identify such
