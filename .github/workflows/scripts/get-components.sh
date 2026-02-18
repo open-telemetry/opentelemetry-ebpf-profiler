9712d07383b86f2165bb0e0e3e257b3e45713e4f
Roger Coll <roger.coll@elastic.co>
2026-02-18 04:48:49 +0100

    Update .github/workflows/scripts/get-components.sh
    Co-authored-by: Christos Kalkanis <christos.kalkanis@elastic.co>

--- .github/workflows/scripts/get-components.sh
+++ .github/workflows/scripts/get-components.sh
@@ -5,7 +5,8 @@
 #
 # Get a list of components within the repository that have some form of ownership
 # ascribed to them.
-
+#
+# Copy of https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/571fce853d044786bd41913765d1f5a813261233/.github/workflows/scripts 
 grep -E '^[A-Za-z0-9/]' .github/CODEOWNERS | \
     awk '{ print $1 }' | \
     sed -E 's%(.+)/$%\1%'
