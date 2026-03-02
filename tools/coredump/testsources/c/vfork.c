c3aa14b7a777ae2cd3e9f523bb5d72da241f1875
hanshal101 <hanshalmehta10@gmail.com>
2026-03-02 16:57:58 +0530

    fix: add the debug prints
    Signed-off-by: hanshal101 <hanshalmehta10@gmail.com>

--- tools/coredump/testsources/c/vfork.c
+++ tools/coredump/testsources/c/vfork.c
@@ -9,16 +9,21 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/prctl.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void trigger_coredump() {
-  // Send SIGILL to ourselves to trigger a coredump.
-  // In a vfork child, this will capture the state while the parent is blocked.
-  kill(getpid(), SIGILL);
-}
+void child_func() {
+  // Allow any process to ptrace us (needed for gcore in some environments)
+  prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
 
-void child_func() { trigger_coredump(); }
+  printf("Child: PID %d, Parent PID %d. Waiting for signal...\n", getpid(),
+         getppid());
+  // Wait for gcore to capture us.
+  while (1) {
+    sleep(1);
+  }
+}
 
 void parent_func() {
   pid_t pid = vfork();
@@ -28,7 +33,9 @@ void parent_func() {
     _exit(0);
   } else if (pid > 0) {
     // Parent process
-    printf("Child finished\n");
+    // Note: Parent is BLOCKED here until child _exits or execs.
+    // So if child loops, parent stays here.
+    printf("Parent: Child PID %d finished\n", pid);
   } else {
     perror("vfork");
   }
