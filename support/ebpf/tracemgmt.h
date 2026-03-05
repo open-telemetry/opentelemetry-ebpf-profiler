a6475d162937a52d463fed38a368aadf0f047906
hanshal101 <hanshalmehta10@gmail.com>
2026-03-05 17:43:24 +0530

    fix: update review
    Signed-off-by: hanshal101 <hanshalmehta10@gmail.com>

--- support/ebpf/tracemgmt.h
+++ support/ebpf/tracemgmt.h
@@ -697,8 +697,8 @@ get_usermode_regs(struct pt_regs *ctx, UnwindState *state, bool *has_usermode_re
 
 #else // TESTING_COREDUMP
 
-static inline EBPF_INLINE
-  ErrorCode get_usermode_regs(struct pt_regs *ctx, UnwindState *state, bool *has_usermode_regs)
+static inline EBPF_INLINE ErrorCode
+get_usermode_regs(struct pt_regs *ctx, UnwindState *state, bool *has_usermode_regs)
 {
   // Coredumps provide always usermode pt_regs directly.
   ErrorCode error = copy_state_regs(state, ctx, false);
