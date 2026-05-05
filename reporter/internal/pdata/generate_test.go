c6a0a641047fb103f3fd3d6f5eb2105d03c05920
Florian Lehner <florianl@users.noreply.github.com>
2026-05-05 07:59:02 +0200

    repoter: remove profcheck workaround (#1394)
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- reporter/internal/pdata/generate_test.go
+++ reporter/internal/pdata/generate_test.go
@@ -847,16 +847,6 @@ func TestGenerate_Validate(t *testing.T) {
 	err = proto.Unmarshal(contents, &data)
 	require.NoError(t, err)
 
-	// Fix for protobuf unmarshaling for ConformanceChecker: The first attribute
-	// table entry must have a nil Value,but protobuf unmarshaling creates a
-	// non-nil but empty AnyValue. Explicitly set it to nil.
-	if data.Dictionary != nil && len(data.Dictionary.AttributeTable) > 0 {
-		firstAttr := data.Dictionary.AttributeTable[0]
-		if firstAttr.KeyStrindex == 0 && firstAttr.UnitStrindex == 0 {
-			firstAttr.Value = nil
-		}
-	}
-
 	err = (profcheck.ConformanceChecker{
 		CheckDictionaryDuplicates: true,
 		CheckSampleTimestampShape: true}).Check(&data)
