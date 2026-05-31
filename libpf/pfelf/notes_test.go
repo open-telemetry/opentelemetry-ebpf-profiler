69b9796d0e56db907b21f228fb0f96eb6257c29e
Timo Teräs <timo.teras@iki.fi>
2026-05-31 12:13:08 +0300

    pfelf: refactor notes and debug link handling to use pfbufio
    Use standard buffering, and refactor notes to use visitor
    pattern and same code everywhere.

--- libpf/pfelf/notes_test.go
+++ libpf/pfelf/notes_test.go
@@ -1,20 +1,20 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package pfelf_test
+package pfelf
 
 import (
+	"bytes"
 	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
-	"go.opentelemetry.io/ebpf-profiler/libpf/pfelf"
 )
 
 func TestGetBuildIDFromNotesFile(t *testing.T) {
-	buildID, err := pfelf.GetBuildIDFromNotesFile("testdata/the_notorious_build_id")
+	r := bytes.NewReader([]byte("\x04\x00\x00\x00\x14\x00\x00\x00\x03\x00\x00\x00GNU\x00_notorious_build_id_"))
+	buildID, err := getBuildIDFromNotesFile(r)
 	require.NoError(t, err)
 	assert.Equal(t, hex.EncodeToString([]byte("_notorious_build_id_")), buildID)
 }
