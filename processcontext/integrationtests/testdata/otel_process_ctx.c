815cf24ef31dd0302f0ff7cb8365386d75fa0c18
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2026-07-11 12:06:19 +0200

    Change otel process ctx writer license

--- processcontext/integrationtests/testdata/otel_process_ctx.c
+++ processcontext/integrationtests/testdata/otel_process_ctx.c
@@ -1,7 +1,7 @@
-// Unless explicitly stated otherwise all files in this repository are licensed
-// under the Apache License (Version 2.0). This product includes software
-// developed at Datadog (https://www.datadoghq.com/) Copyright 2025 Datadog,
-// Inc.
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Source: https://github.com/open-telemetry/sig-profiling/blob/main/process-context/c-and-cpp/otel_process_ctx.c
 
 #include "otel_process_ctx.h"
 
