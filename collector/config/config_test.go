8a1c86f45dbec6a44e0b5bdaeed56db0b2899b5c
Florian Lehner <florianl@users.noreply.github.com>
2026-07-15 22:49:11 +0200

    OTel: ensure cross-compilation support (#1632)
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- collector/config/config_test.go
+++ collector/config/config_test.go
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build linux && (amd64 || arm64)
+
 package config // import "go.opentelemetry.io/ebpf-profiler/collector/config"
 
 import (
