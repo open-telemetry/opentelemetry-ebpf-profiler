e881d3fd051c0813a1d390d99a0f16121f92e010
Florian Lehner <florian.lehner@elastic.co>
2026-07-21 10:42:35 +0200

    process: cache proc base path
    Using fmt.Sprintf() is expensive, especially for repeated information. Looking
    at flamegraphs, I noticed we call GetExe() and GetProcessMeta() on a regular
    base, where we construct certain paths using fmt.Sprintf().
    
    Caching the proc base path reduced allocations and speed up these functions.
    
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- process/debug_linux.go
+++ process/debug_linux.go
@@ -52,6 +52,7 @@ func NewPtrace(pid libpf.PID) (Process, error) {
 
 	sp := &ptraceProcess{}
 	sp.pid = pid
+	sp.procBase = "/proc/" + strconv.Itoa(int(pid))
 	sp.remoteMemory = remotememory.RemoteMemory{ReaderAt: sp}
 	if err := sp.attach(); err != nil {
 		runtime.UnlockOSThread()
@@ -61,7 +62,7 @@ func NewPtrace(pid libpf.PID) (Process, error) {
 }
 
 func (sp *ptraceProcess) GetThreads() ([]ThreadInfo, error) {
-	tidFiles, err := os.ReadDir(fmt.Sprintf("/proc/%d/task", sp.pid))
+	tidFiles, err := os.ReadDir(sp.procBase + "/task")
 	if err != nil {
 		return nil, err
 	}
