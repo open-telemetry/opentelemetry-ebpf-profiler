6e3a5d74d609240a003dc8b001145c3d5b25ee98
Martin Levesque <113706003+wehzzz@users.noreply.github.com>
2026-08-13 16:25:36 +0200

    feat: add support for asmcgo unwindind on arm64 (#1626)
    Co-authored-by: Florian Lehner <florianl@users.noreply.github.com>
    Co-authored-by: Timo Teräs <timo.teras@iki.fi>

--- support/types_def.go
+++ support/types_def.go
@@ -164,6 +164,7 @@ const (
 	UnwindCommandPLT          int32 = C.UNWIND_COMMAND_PLT
 	UnwindCommandSignal       int32 = C.UNWIND_COMMAND_SIGNAL
 	UnwindCommandFramePointer int32 = C.UNWIND_COMMAND_FRAME_POINTER
+	UnwindCommandGoAsmcgocall int32 = C.UNWIND_COMMAND_GO_ASMCGOCALL
 
 	// UnwindDeref handling from the C header file
 	UnwindDerefMask       int32 = C.UNWIND_DEREF_MASK
@@ -310,4 +311,7 @@ var MetricsTranslation = []metrics.MetricID{
 	C.metricID_SamplesSkippedProcessTooNew:                metrics.IDSamplesSkippedProcessTooNew,
 	C.metricID_NumSyncsFromPrctl:                          metrics.IDNumSyncsFromPrctl,
 	C.metricID_NumPriorityEventDeferred:                   metrics.IDNumPriorityEventDeferred,
+	C.metricID_UnwindGoAsmcgocallAttempts:                 metrics.IDUnwindGoAsmcgocallAttempts,
+	C.metricID_UnwindGoAsmcgocallSuccess:                  metrics.IDUnwindGoAsmcgocallSuccess,
+	C.metricID_UnwindGoAsmcgocallUnwindFailure:            metrics.IDUnwindGoAsmcgocallUnwindFailure,
 }
