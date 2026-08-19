fdad78d663e25b5e99eadb471f5bba85b1714b8d
Florian Lehner <florian.lehner@elastic.co>
2026-08-19 13:53:03 +0200

    fix issues introduced by git merge main
    Signed-off-by: Florian Lehner <florian.lehner@elastic.co>

--- process/process_test.go
+++ process/process_test.go
@@ -24,8 +24,8 @@ var testMappings = `
 55fe827be000-55fe82836000 r--p 000ae000 fd:01 1068432                    /tmp/usr_bin_seahorse
 55fe82836000-55fe8283d000 r--p 00125000 fd:01 1068432                    /tmp/usr_bin_seahorse
 55fe8283d000-55fe8283e000 rw-p 0012c000 fd:01 1068432                    /tmp/usr_bin_seahorse
-7f63c8c3e000-7f63c8de0000 r-xp 00085000 08:01 1048922                    /tmp/usr_lib_x86_64-linux-gnu_libcrypto.so.1.1
-7f63c8ebf000-7f63c8fef000 r-xp 0001c000 1fd:2ff 1075944                  /tmp/usr_lib_x86_64-linux-gnu_libopensc.so.6.0.0
+7f63c8c3e000-7f63c8de0000 r-xp 00085000 08:01 1048922                    /tmp/libcrypto.so.1.1
+7f63c8ebf000-7f63c8fef000 r-xp 0001c000 1fd:2ff 1075944                  /tmp/libopensc.so.6.0.0
 7f63c8eef000-7f63c8fdf000 r-xp 0001c000 1fd:01
 7f63c8eef000-7f63c8fdf000 r-xp 0001c000 1fd.01 1075944
 7f63c8eef000-7f63c8fdf000 r- 0001c000 1fd:01 1075944
@@ -86,7 +86,7 @@ var allExpectedMappings = []RawMapping{
 		Inode:      1048922,
 		Length:     0x1A2000,
 		FileOffset: 544768,
-		Path:       "/tmp/usr_lib_x86_64-linux-gnu_libcrypto.so.1.1",
+		Path:       "/tmp/libcrypto.so.1.1",
 	},
 	{
 		Vaddr:      0x7f63c8ebf000,
@@ -95,7 +95,7 @@ var allExpectedMappings = []RawMapping{
 		Inode:      1075944,
 		Length:     0x130000,
 		FileOffset: 114688,
-		Path:       "/tmp/usr_lib_x86_64-linux-gnu_libopensc.so.6.0.0",
+		Path:       "/tmp/libopensc.so.6.0.0",
 	},
 	{
 		Vaddr:      0x7f8b929f0000,
